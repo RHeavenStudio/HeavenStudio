885f6297225a51856e4d41ef7a81d97af86c39c8
minenice55 <star.elementa@gmail.com>
2022-04-24 17:23:39 -0400

    Samurai Slice (Gold) - Basic interactions

--- Assets/Scripts/Games/SamuraiSliceNtr/SamuraiSliceNtr.cs
+++ Assets/Scripts/Games/SamuraiSliceNtr/SamuraiSliceNtr.cs
@@ -1,3 +1,4 @@
+using HeavenStudio.Util;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,7 @@ namespace HeavenStudio.Games.Loaders
         public static Minigame AddGame(EventCaller eventCaller) {
             return new Minigame("samuraiSliceNtr", "Samurai Slice (DS) \n<color=#eb5454>[WIP don't use]</color>", "000000", false, false, new List<GameAction>()
             {
+                //new GameAction("start bopping",                   delegate { SamuraiSliceNtr.instance.Bop(eventCaller.currentEntity.beat, eventCaller.currentEntity.length); }, 1),
             });
         }
     }
@@ -17,26 +19,59 @@ namespace HeavenStudio.Games.Loaders
 
 namespace HeavenStudio.Games
 {
+    using Scripts_NtrSamurai;
+
     public class SamuraiSliceNtr : Minigame
     {
+        [Header("References")]
+        public NtrSamurai player;
+        public GameObject launcher;
+
         //game scene
         public static SamuraiSliceNtr instance;
+
+        public GameEvent bop = new GameEvent();
 
         private void Awake()
         {
             instance = this;
         }
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
-
         // Update is called once per frame
         void Update()
         {
-            
+            var cond = Conductor.instance;
+            if (cond.ReportBeat(ref bop.lastReportedBeat, bop.startBeat % 1))
+            {
+                player.Bop();
+            }
+
+            if (PlayerInput.AltPressed())
+            {
+                Jukebox.PlayOneShotGame("samuraiSliceNtr/ntrSamurai_launchThrough");
+                player.Step(false);
+                launcher.GetComponent<Animator>().Play("Launch", -1, 0);
+            }
+            if (PlayerInput.AltPressedUp() && player.isStepping())
+            {
+                player.Step(true);
+                launcher.GetComponent<Animator>().Play("UnStep", -1, 0);
+            }
+            if (PlayerInput.Pressed())
+            {
+                if (player.isStepping())
+                {
+                    launcher.GetComponent<Animator>().Play("UnStep", -1, 0);
+                }
+                Jukebox.PlayOneShotGame("samuraiSliceNtr/ntrSamurai_through");
+                player.Slash();
+            }
+        }
+
+        public void Bop(float beat, float length) 
+        {
+            bop.length = length;
+            bop.startBeat = beat;
         }
     }
 }
