d5d136089cf6c641b9c815d838f402eb3a649fae
minenice55 <star.elementa@gmail.com>
2022-05-19 18:34:29 -0400

    Pajama Party: thrown pillow, prep sequences

--- Assets/Scripts/Games/PajamaParty/CtrPillowPlayer.cs
+++ Assets/Scripts/Games/PajamaParty/CtrPillowPlayer.cs
@@ -12,6 +12,7 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
         [Header("Objects")]
         public GameObject Player;
         public GameObject Shadow;
+        public GameObject Projectile;
 
         public Animator anim;
         float startJumpTime = Single.MinValue;
@@ -23,6 +24,13 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
 
         private bool charging = false;
         private bool canCharge = true;
+
+        float startThrowTime = Single.MinValue;
+        float throwLength = 0;
+        float throwHeight = 0;
+        // true = throw, false = dropped ("Out")
+        bool throwType = true;
+        bool hasThrown = false;
 
         void Awake()
         {
@@ -49,15 +57,16 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
                 }
             //
 
+            // mako jumping logic
             float jumpPos = cond.GetPositionFromBeat(startJumpTime, jumpLength);
             if (jumpPos >= 0 && jumpPos <= 1f)
             {
                 hasJumped = true;
                 float yMul = jumpPos * 2f - 1f;
                 float yWeight = -(yMul*yMul) + 1f;
-                Player.transform.localPosition = new Vector3(0, jumpHeight * yWeight + 0.25f);
+                Player.transform.localPosition = new Vector3(0, jumpHeight * yWeight);
                 Shadow.transform.localScale = new Vector3((1f-yWeight*0.2f) * 1.65f, (1f-yWeight*0.2f), 1f);
-
+                // handles the shirt lifting
                 anim.Play("MakoJump", 0, jumpPos);
                 anim.speed = 0;
             }
@@ -75,6 +84,60 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
                 startJumpTime = Single.MinValue;
                 Player.transform.localPosition = new Vector3(0, 0);
                 Shadow.transform.localScale = new Vector3(1.65f, 1f, 1f);
+            }
+
+            //thrown pillow logic
+            jumpPos = cond.GetPositionFromBeat(startThrowTime, throwLength);
+            if (jumpPos >= 0 && jumpPos <= 1f)
+            {
+                hasThrown = true;
+                float yMul = jumpPos * 2f - 1f;
+                float yWeight = -(yMul*yMul) + 1f;
+                Projectile.transform.localPosition = new Vector3(0, throwHeight * yWeight + 0.5f);
+                Projectile.transform.rotation = Quaternion.Euler(0, 0, Projectile.transform.rotation.eulerAngles.z - (360f * Time.deltaTime));
+            }
+            else
+            {
+                startThrowTime = Single.MinValue;
+                Projectile.transform.localPosition = new Vector3(0, 0);
+                Projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
+                if (hasThrown)
+                {
+                    if (throwLength == 1f)    //ew hardcoded change later
+                    {
+                        anim.Play("MakoCatchNg", -1, 0);
+                        //TODO: change when locales are a thing
+                        Jukebox.PlayOneShotGame("pajamaParty/jp/catchNg" + UnityEngine.Random.Range(0, 2)); //bruh
+                    }
+                    else
+                    {
+                        anim.Play("MakoCatch", -1, 0);
+                        //TODO: change when locales are a thing
+                        Jukebox.PlayOneShotGame("pajamaParty/jp/catchJust");
+                    }
+                    anim.speed = 1f;
+                    Projectile.SetActive(false);
+                    hasThrown = false;
+
+
+                    canCharge = true;
+                    canJump = true;
+                }
+            }
+        }
+
+        public void ProjectileThrow(float beat, bool drop = false, bool ng = false)
+        {
+            if (drop)
+            {
+                // fuckkkk have to animate the pillow bouncing
+            }
+            else
+            {
+                Projectile.SetActive(true);
+                startThrowTime = beat;
+                throwHeight = ng ? 1.5f : 12f;
+                throwLength = ng ? 1f : 4f;
             }
         }
 
@@ -97,8 +160,9 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
             charging = true;
         }
 
-        public void EndCharge(float beat, bool hit = false)
+        public void EndCharge(float beat, bool hit = true, bool ng = false)
         {
+            ProjectileThrow(beat, !hit, ng);
             var cond = Conductor.instance;
             charging = false;
             canCharge = false;
@@ -106,20 +170,6 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
             {
                 anim.Play("MakoThrow");
                 anim.speed = 1f; 
-                //TODO: spawn projectile
-
-                BeatAction.New(Player, new List<BeatAction.Action>()
-                {
-                    new BeatAction.Action(
-                        beat + 4f,
-                        delegate { 
-                            anim.Play("MakoCatch", -1, 0); 
-                            anim.speed = 1f; 
-                            canCharge = true;
-                            canJump = true;
-                        }
-                    ),
-                });
             }
             else
             {
@@ -132,17 +182,44 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
                         delegate { 
                             anim.Play("MakoPickUp", -1, 0);
                             anim.speed = 1f / cond.pitchedSecPerBeat;
-                        }
-                    ),
-                    new BeatAction.Action(
-                        beat + 1f,
-                        delegate { 
+                            
                             canCharge = true;
                             canJump = true;
                         }
-                    ),
+                    )
                 });
             }
+        }
+
+        public void StartSleepSequence(float beat)
+        {
+            var cond = Conductor.instance;
+            charging = false;
+            canCharge = false;
+            canJump = false;
+            if (hasJumped)
+            {
+                canJump = true;
+                canCharge = true;
+                hasJumped = false;
+                PajamaParty.instance.DoBedImpact();
+                anim.Play("MakoLand", -1, 0);
+                anim.speed = 1f / cond.pitchedSecPerBeat;
+            }
+            startJumpTime = Single.MinValue;
+            Player.transform.localPosition = new Vector3(0, 0);
+            Shadow.transform.localScale = new Vector3(1.65f, 1f, 1f);
+
+            BeatAction.New(Player, new List<BeatAction.Action>()
+            {
+                new BeatAction.Action(
+                    beat + 8f,
+                    delegate { 
+                        canCharge = true;
+                        canJump = true;
+                    }
+                ),
+            });
         }
     }
 }
--- Assets/Scripts/Games/PajamaParty/PajamaParty.cs
+++ Assets/Scripts/Games/PajamaParty/PajamaParty.cs
@@ -14,14 +14,14 @@ namespace HeavenStudio.Games.Loaders
             return new Minigame("pajamaParty", "Pajama Party \n<color=#eb5454>[WIP don't use]</color>", "000000", false, false, new List<GameAction>()
             {
                 // both same timing
-                new GameAction("jump (side to middle)",     delegate { }, 4f, true),
-                new GameAction("jump (back to front)",      delegate { }, 4f, true),
+                new GameAction("jump (side to middle)",     delegate {PajamaParty.instance.DoThreeJump(eventCaller.currentEntity.beat);}, 4f, false),
+                new GameAction("jump (back to front)",      delegate {PajamaParty.instance.DoFiveJump(eventCaller.currentEntity.beat);}, 4f, false),
                 //idem
-                new GameAction("slumber",                   delegate {PajamaParty.instance.DoSleepSequence(eventCaller.currentEntity.beat);}, 8f, true),
-                new GameAction("throw",                     delegate { }, 8f, true),
+                new GameAction("slumber",                   delegate {PajamaParty.instance.DoSleepSequence(eventCaller.currentEntity.beat);}, 8f, false),
+                new GameAction("throw",                     delegate { }, 8f, false),
                 //cosmetic
                 new GameAction("open / close background",   delegate { }, 2f, true),
-                // do shit with mako's face?
+                // do shit with mako's face? (talking?)
             });
         }
     }
@@ -49,36 +49,67 @@ namespace HeavenStudio.Games
             
         }
 
+        public void DoThreeJump(float beat)
+        {
+            MultiSound.Play(new MultiSound.Sound[] { 
+                new MultiSound.Sound("pajamaParty/three1", beat), 
+                new MultiSound.Sound("pajamaParty/three2", beat + 1f),
+                new MultiSound.Sound("pajamaParty/three3", beat + 2f),
+            });
+        }
+
+        public void DoFiveJump(float beat)
+        {
+            MultiSound.Play(new MultiSound.Sound[] { 
+                new MultiSound.Sound("pajamaParty/five1", beat), 
+                new MultiSound.Sound("pajamaParty/five2", beat + 0.5f),
+                new MultiSound.Sound("pajamaParty/five3", beat + 1f),
+                new MultiSound.Sound("pajamaParty/five4", beat + 1.5f),
+                new MultiSound.Sound("pajamaParty/five5", beat + 2f)
+            });
+        }
+
         public void DoSleepSequence(float beat)
         {
             var cond = Conductor.instance;
+            Mako.StartSleepSequence(beat);
+
+            MultiSound.Play(new MultiSound.Sound[] { 
+                new MultiSound.Sound("pajamaParty/siesta1", beat), 
+                new MultiSound.Sound("pajamaParty/siesta2", beat + 0.5f),
+                new MultiSound.Sound("pajamaParty/siesta3", beat + 1f),
+                new MultiSound.Sound("pajamaParty/siesta4", beat + 2.5f),
+                new MultiSound.Sound("pajamaParty/siesta5", beat + 4f)
+            });
+
             BeatAction.New(Mako.Player, new List<BeatAction.Action>()
             {
                 new BeatAction.Action(
                     beat,
                     delegate { Mako.anim.Play("MakoSleep00", -1, 0); 
-                            Mako.anim.speed = 1f / cond.pitchedSecPerBeat; 
+                            Mako.anim.speed = 1f / cond.pitchedSecPerBeat;
                         }
                 ),
                 new BeatAction.Action(
                     beat + 0.5f,
                     delegate { Mako.anim.Play("MakoSleep01", -1, 0); 
-                            Mako.anim.speed = 1f; 
+                            Mako.anim.speed = 1f;
                         }
                 ),
                 new BeatAction.Action(
                     beat + 3,
                     delegate { Mako.anim.Play("MakoReadySleep", -1, 0); 
-                            Mako.anim.speed = 1f / cond.pitchedSecPerBeat; 
+                            Mako.anim.speed = 1f / cond.pitchedSecPerBeat;
                         }
                 ),
-                // test
-                new BeatAction.Action(
-                    beat + 4f,
-                    delegate { Mako.anim.Play("MakoSleepJust", -1, 0); 
-                            Mako.anim.speed = 1f; 
-                        }
-                ),
+                //test
+                    new BeatAction.Action(
+                        beat + 4,
+                        delegate { Mako.anim.Play("MakoSleepJust", -1, 0); 
+                                Mako.anim.speed = 1f;
+                            }
+                    ),
+                //
             });
         }
 
