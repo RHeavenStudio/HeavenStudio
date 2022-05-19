aca4b69c46a3ac7c168504432b9d5fac5082f24b
minenice55 <star.elementa@gmail.com>
2022-05-19 16:46:20 -0400

    Pajama Party: All Mako anims, add sounds to fs

--- Assets/Scripts/Games/PajamaParty/CtrPillowPlayer.cs
+++ Assets/Scripts/Games/PajamaParty/CtrPillowPlayer.cs
@@ -17,7 +17,12 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
         float startJumpTime = Single.MinValue;
         float jumpLength = 0;
         float jumpHeight = 0;
+
         private bool hasJumped = false;
+        private bool canJump = true;
+
+        private bool charging = false;
+        private bool canCharge = true;
 
         void Awake()
         {
@@ -28,11 +33,21 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
         void Update()
         {
             var cond = Conductor.instance;
+
             // TESTING REMOVE FOR PROD
-            if (PlayerInput.Pressed())
-            {
-                PlayerJump(cond.songPositionInBeats);
-            }
+                if (PlayerInput.Pressed() && canJump)
+                {
+                    PlayerJump(cond.songPositionInBeats);
+                }
+                if (PlayerInput.AltPressed() && canCharge)
+                {
+                    StartCharge();
+                }
+                if (PlayerInput.AltPressedUp() && charging)
+                {
+                    EndCharge(cond.songPositionInBeats);
+                }
+            //
 
             float jumpPos = cond.GetPositionFromBeat(startJumpTime, jumpLength);
             if (jumpPos >= 0 && jumpPos <= 1f)
@@ -50,6 +65,8 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
             {
                 if (hasJumped)
                 {
+                    canJump = true;
+                    canCharge = true;
                     hasJumped = false;
                     PajamaParty.instance.DoBedImpact();
                     anim.Play("MakoLand", -1, 0);
@@ -64,10 +81,68 @@ namespace HeavenStudio.Games.Scripts_PajamaParty
         public void PlayerJump(float beat)
         {
             startJumpTime = beat;
+            canCharge = false;
+            canJump = false;
 
             //temp
             jumpLength = 1f;
             jumpHeight = 4f;
+        }
+
+        public void StartCharge()
+        {
+            canJump = false;
+            anim.Play("MakoReady");
+            anim.speed = 1f;
+            charging = true;
+        }
+
+        public void EndCharge(float beat, bool hit = false)
+        {
+            var cond = Conductor.instance;
+            charging = false;
+            canCharge = false;
+            if (hit)
+            {
+                anim.Play("MakoThrow");
+                anim.speed = 1f; 
+                //TODO: spawn projectile
+
+                BeatAction.New(Player, new List<BeatAction.Action>()
+                {
+                    new BeatAction.Action(
+                        beat + 4f,
+                        delegate { 
+                            anim.Play("MakoCatch", -1, 0); 
+                            anim.speed = 1f; 
+                            canCharge = true;
+                            canJump = true;
+                        }
+                    ),
+                });
+            }
+            else
+            {
+                anim.Play("MakoThrowOut");
+                anim.speed = 1f / cond.pitchedSecPerBeat;
+                BeatAction.New(Player, new List<BeatAction.Action>()
+                {
+                    new BeatAction.Action(
+                        beat + 0.5f,
+                        delegate { 
+                            anim.Play("MakoPickUp", -1, 0);
+                            anim.speed = 1f / cond.pitchedSecPerBeat;
+                        }
+                    ),
+                    new BeatAction.Action(
+                        beat + 1f,
+                        delegate { 
+                            canCharge = true;
+                            canJump = true;
+                        }
+                    ),
+                });
+            }
         }
     }
 }
