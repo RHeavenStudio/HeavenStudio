0241cd0533ccab9bc43fef72b6ae9f5879e8e115
ThatZeoMan <67521686+ThatZeoMan@users.noreply.github.com>
2022-08-21 19:34:45 -0500

    Menu Fully Functional!
    There's not much on it right now, but it's nice to have

--- Assets/Scripts/Menu/LogoAnimator.cs
+++ Assets/Scripts/Menu/LogoAnimator.cs
@@ -5,19 +5,37 @@ using UnityEngine;
 
 public class LogoAnimator : MonoBehaviour
 {
+    public float currentBeatRem = 0;
+    public float lastBeatRem = 0;
+    public bool beatSide = false;   // false = right, true = left
+
     // Start is called before the first frame update
     void Start()
     {
-         Animator animator = gameObject.GetComponent<Animator>();
-         animator.Play("Beat");
+         
     }
 
     void Update()
     {
-        if (HeavenStudio.MenuConductor.instance.songPositionInBeats % 1f == 0f)
+        Animator animator = gameObject.GetComponent<Animator>();
+
+        currentBeatRem = HeavenStudio.MenuConductor.instance.songPositionInBeats % 1;
+
+        if (currentBeatRem < lastBeatRem & beatSide == false)
         {
-            //animator.Play("Beat");
+            animator.Play("BeatR");
+            beatSide = true;
             Debug.Log("Beat");
+            //animator.Play("idle");
         }
+        else if (currentBeatRem < lastBeatRem & beatSide == true)
+        {
+            animator.Play("BeatL");
+            beatSide = false;
+            Debug.Log("BeatL");
+            //animator.Play("idle");
+        }
+
+        lastBeatRem = HeavenStudio.MenuConductor.instance.songPositionInBeats % 1;
     }
 }
