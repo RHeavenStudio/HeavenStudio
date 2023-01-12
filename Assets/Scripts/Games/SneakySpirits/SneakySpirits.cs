44a53cc353495f7a500ac129f98143e27b37925a
Amy54Desu <69287652+Amy54Desu@users.noreply.github.com>
2023-01-11 20:42:56 -0500

    Sneaky Spirits
    Start to work on Sneaky Spririts GBA

--- Assets/Scripts/Games/SneakySpirits/SneakySpirits.cs
+++ Assets/Scripts/Games/SneakySpirits/SneakySpirits.cs
@@ -1,3 +1,4 @@
+using HeavenStudio.Games.Scripts_SneakySpirits;
 using HeavenStudio.Util;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,21 @@ namespace HeavenStudio.Games.Loaders
         public static Minigame AddGame(EventCaller eventCaller) {
             return new Minigame("sneakySpirits", "Sneaky Spirits \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "0058CE", false, false, new List<GameAction>()
             {
-            });
+                new GameAction("spawn spirit", "Spawn Spirit")
+                {
+                    defaultLength = 1f,
+                    function = delegate { var e = eventCaller.currentEntity; SneakySpirits.instance.SpawnSpirit(e.beat, e["valA"]); },
+                    parameters =
+                    {
+                        new Param("valA", new EntityTypes.Integer(1, 8, 8), "Length", "The length until you have to shoot the spirit")
+                    }
+                }
+            },
+            new List<string>() { "agb", "aim" },
+            "agbobake", "en", "ver0",
+            new List<string>() {},
+            new List<string>() {}
+            );
         }
     }
 }
@@ -21,16 +36,42 @@ namespace HeavenStudio.Games
     //using Scripts_SneakySpirits;
     public class SneakySpirits : Minigame
     {
+        public static SneakySpirits instance;
+
+        // references
+        [Header("References")]
+        public Transform SpawnRoot;
+        public GameObject SneakySpirit;
+        public GameObject Arrow;
+
         // Start is called before the first frame update
         void Awake()
         {
-            
+            instance = this;
         }
 
         // Update is called once per frame
         void Update()
         {
             
+        }
+
+        public void SpawnSpirit(float beat, int length)
+        {
+            CreateSpirit(beat, length);//, )
+        }
+
+        public GameObject CreateSpirit(float beat, int length)//, string awakeAnim)
+        {
+            GameObject mobj = GameObject.Instantiate(SneakySpirit, SpawnRoot);
+            WahSpirits mobjDat = mobj.GetComponent<WahSpirits>();
+
+            mobjDat.startBeat = beat;
+            mobjDat.numBeats = length;
+
+            mobj.SetActive(true);
+
+            return mobj;
         }
     }
 }
---
+++ Assets/Scripts/Games/SneakySpirits/WahSpirits.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NaughtyBezierCurves;
+
+using HeavenStudio.Util;
+using System;
+
+namespace HeavenStudio.Games.Scripts_SneakySpirits
+{
+    public class WahSpirits : PlayerActionObject
+    {
+        public float startBeat;
+        public int numBeats;
+
+        // Start is called before the first frame update
+        PlayerActionEvent OnHit;
+        void Awake()
+        {
+            OnHit = SneakySpirits.instance.ScheduleInput(startBeat, numBeats, InputType.STANDARD_DOWN, SpiritHitOrNG, SpiritMiss, SpiritEmpty);
+        }
+
+        public void SpiritHitOrNG(PlayerActionEvent caller, float state)
+        {
+            if (GameManager.instance.currentGame != "sneakySpirits") return;
+            //hitting a normal object with the alt input
+            //WHEN SCORING THIS IS A MISS
+            if (state <= -1f || state >= 1f)
+            {
+                startBeat = Conductor.instance.songPositionInBeats;
+                Jukebox.PlayOneShot("miss");
+                SpiritNG();
+            }
+            else
+            {
+                SpiritHit();
+            }
+            OnHit.CanHit(false);
+        }
+
+        public void SpiritMiss(PlayerActionEvent caller)
+        {
+            Animator mobjAnim = GetComponent<Animator>();
+            mobjAnim.Play("Miss", -1, 0);
+        }
+
+        public void SpiritNG()
+        {
+
+        }
+
+        public void SpiritHit()
+        {
+
+        }
+
+        public void SpiritEmpty(PlayerActionEvent caller) { }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
