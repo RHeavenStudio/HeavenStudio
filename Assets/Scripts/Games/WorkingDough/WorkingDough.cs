b972c219287af567d966f974493ef081530527d6
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-01-14 16:25:01 +0100

    Added Audio and Small and Big Ball events

--- Assets/Scripts/Games/WorkingDough/WorkingDough.cs
+++ Assets/Scripts/Games/WorkingDough/WorkingDough.cs
@@ -18,6 +18,16 @@ namespace HeavenStudio.Games.Loaders
                     defaultLength = 4f,
                     resizable = true
                 },
+                new GameAction("small ball", "Small Ball")
+                {
+                    function = delegate { WorkingDough.instance.SpawnBall(eventCaller.currentEntity.beat, false);  },
+                    defaultLength = 1f,
+                },
+                new GameAction("big ball", "Big Ball")
+                {
+                    function = delegate { WorkingDough.instance.SpawnBall(eventCaller.currentEntity.beat, true);  },
+                    defaultLength = 1f,
+                },
             });
         }
     }
@@ -35,6 +45,8 @@ namespace HeavenStudio.Games
         [SerializeField] GameObject ballTransporterLeftNPC; //Close and open animations
         [SerializeField] GameObject ballTransporterRightPlayer; //Close and open animations
         [SerializeField] GameObject ballTransporterLeftPlayer; //Close and open animations
+        [SerializeField] GameObject npcImpact;
+        [SerializeField] GameObject playerImpact;
 
         [Header("Variables")]
         public bool intervalStarted;
@@ -58,14 +70,14 @@ namespace HeavenStudio.Games
                 ballTransporterRightNPC.GetComponent<Animator>().Play("BallTransporterRightOpen", 0, 0);
                 BeatAction.New(ballTransporterLeftNPC, new List<BeatAction.Action>()
                 {
+                    //End interval
+                    new BeatAction.Action(beat + interval, delegate { intervalStarted = false; }),
                     //Close npc transporters
                     new BeatAction.Action(beat + interval, delegate { ballTransporterLeftNPC.GetComponent<Animator>().Play("BallTransporterLeftClose", 0, 0); }),
                     new BeatAction.Action(beat + interval, delegate { ballTransporterRightNPC.GetComponent<Animator>().Play("BallTransporterRightClose", 0, 0); }),
                     //Open player transporters
                     new BeatAction.Action(beat + interval, delegate { ballTransporterLeftPlayer.GetComponent<Animator>().Play("BallTransporterLeftOpen", 0, 0); }),
                     new BeatAction.Action(beat + interval, delegate { ballTransporterRightPlayer.GetComponent<Animator>().Play("BallTransporterRightOpen", 0, 0); }),
-                    //End interval
-                    new BeatAction.Action(beat + interval, delegate { intervalStarted = false; }),
                     //Close player transporters
                     new BeatAction.Action(beat + interval * 2, delegate { ballTransporterLeftPlayer.GetComponent<Animator>().Play("BallTransporterLeftClose", 0, 0); }),
                     new BeatAction.Action(beat + interval * 2, delegate { ballTransporterRightPlayer.GetComponent<Animator>().Play("BallTransporterRightClose", 0, 0); }),
@@ -76,9 +88,46 @@ namespace HeavenStudio.Games
             beatInterval = interval;
         }
 
+        public void SpawnBall(float beat, bool isBig)
+        {
+            if (isBig)
+            {
+                BeatAction.New(doughDudesNPC, new List<BeatAction.Action>()
+                {
+                    //Jump and play sound
+                    new BeatAction.Action(beat + 0.5f, delegate { doughDudesNPC.GetComponent<Animator>().Play("BigDoughJump", 0, 0); }),
+                    new BeatAction.Action(beat + 0.5f, delegate { Jukebox.PlayOneShotGame("workingDough/NPCBigBall"); }),
+                    new BeatAction.Action(beat + 0.5f, delegate { Jukebox.PlayOneShotGame("workingDough/SmallBall"); }),
+                    new BeatAction.Action(beat + 0.5f, delegate { npcImpact.SetActive(true); }),
+                    new BeatAction.Action(beat + 0.6f, delegate { npcImpact.SetActive(false); }),
+                });
+            }
+            else
+            {
+                BeatAction.New(doughDudesNPC, new List<BeatAction.Action>()
+                {
+                    //Jump and play sound
+                    new BeatAction.Action(beat + 0.5f, delegate { doughDudesNPC.GetComponent<Animator>().Play("SmallDoughJump", 0, 0); }),
+                    new BeatAction.Action(beat + 0.5f, delegate { Jukebox.PlayOneShotGame("workingDough/NPCSmallBall"); }),
+                    new BeatAction.Action(beat + 0.5f, delegate { Jukebox.PlayOneShotGame("workingDough/SmallBall"); }),
+                    new BeatAction.Action(beat + 0.5f, delegate { npcImpact.SetActive(true); }),
+                    new BeatAction.Action(beat + 0.6f, delegate { npcImpact.SetActive(false); }),
+                });
+            }
+        }
+
         void Update()
         {
-
+            if(PlayerInput.Pressed())
+            {
+                doughDudesPlayer.GetComponent<Animator>().Play("SmallDoughJump", 0, 0);
+                Jukebox.PlayOneShotGame("workingDough/PlayerSmallBall");
+            }
+            else if (PlayerInput.AltPressed())
+            {
+                doughDudesPlayer.GetComponent<Animator>().Play("BigDoughJump", 0, 0);
+                Jukebox.PlayOneShotGame("workingDough/PlayerBigBall");
+            }
         }
     }
 }
