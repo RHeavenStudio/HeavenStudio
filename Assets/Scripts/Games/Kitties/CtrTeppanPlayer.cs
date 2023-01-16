bfa8527a50e90144553c12e71a9a0ff6f6096c71
Bhaos <malaslanbook@gmail.com>
2023-01-16 06:38:27 -0600

    Kitties (SFX added and some work on input)

---
+++ Assets/Scripts/Games/Kitties/CtrTeppanPlayer.cs
@@ -0,0 +1,76 @@
+using HeavenStudio.Util;
+using System.Collections;
+using System.Collections.Generic;
+
+using System;
+using UnityEngine;
+
+namespace HeavenStudio.Games.Scripts_Kitties
+{
+    public class CtrTeppanPlayer : MonoBehaviour
+    {
+        [Header("Objects")]
+        public GameObject Player;
+        public Animator anim;
+        private int spawnType;
+
+        private bool hasClapped = false;
+        public bool canClap = false;
+
+        private bool hasSpun = false;
+        private bool canSpin = true;
+
+        private bool hasFish = false;
+        private bool canFish = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var cond = Conductor.instance;
+
+            if (PlayerInput.Pressed() && canClap && !Kitties.instance.IsExpectingInputNow(InputType.STANDARD_DOWN))
+            {
+                Jukebox.PlayOneShot("miss");
+            }
+        }
+
+        public void ScheduleClap(float beat)
+        {
+            Kitties.instance.ScheduleInput(beat, 2.5f, InputType.STANDARD_DOWN, ClapSuccessOne, ClapMissOne, ClapEmpty);
+            Kitties.instance.ScheduleInput(beat, 3f, InputType.STANDARD_DOWN, ClapSuccessTwo, ClapMissTwo, ClapEmpty);
+        }
+
+        public void ClapSuccessOne(PlayerActionEvent Caller, float state)
+        {
+            if (state >= 1f || state <= -1f)
+            {  //todo: proper near miss feedback
+                Jukebox.PlayOneShotGame("kitties/clapMiss1");
+            }
+            else
+                Jukebox.PlayOneShotGame("kitties/clap1");
+        }
+        public void ClapMissOne(PlayerActionEvent Caller)
+        {
+            Jukebox.PlayOneShotGame("kitties/clapMiss1");
+        }
+        public void ClapEmpty(PlayerActionEvent Caller)
+        {
+
+        }
+
+        public void ClapSuccessTwo(PlayerActionEvent Caller, float state)
+        {
+            Jukebox.PlayOneShotGame("kitties/clap2");
+        }
+
+        public void ClapMissTwo(PlayerActionEvent Caller)
+        {
+            Jukebox.PlayOneShotGame("kitties/clapMiss2");
+        }
+    }
+}
--- Assets/Scripts/Games/Kitties/Kitties.cs
+++ Assets/Scripts/Games/Kitties/Kitties.cs
@@ -17,7 +17,7 @@ namespace HeavenStudio.Games.Loaders
                     function = delegate { Kitties.instance.Clap(eventCaller.currentEntity["toggle"], eventCaller.currentEntity["toggle"], 
                         eventCaller.currentEntity.beat,  eventCaller.currentEntity["type"]); },
 
-                    defaultLength = 3,
+                    defaultLength = 4f,
                     parameters = new List<Param>()
                     {
                         new Param("type", Kitties.SpawnType.Straight, "Spawn", "The way in which the kitties will spawn"),
@@ -26,27 +26,29 @@ namespace HeavenStudio.Games.Loaders
                     }
                 },
 
-                new GameAction("clapclose", "Up Close Clap")
+                new GameAction("remove", "Remove Cats")
                 {
-                    function = delegate { Kitties.instance.ClapClose(eventCaller.currentEntity.beat); },
+                    function = delegate { Kitties.instance.Remove(eventCaller.currentEntity.beat); },
 
-                    defaultLength = 3,
-                },
-
+                    defaultLength = .5f,
+                }
             });
         }
     }
 }
 namespace HeavenStudio.Games
 {
-    using Scripts_Fireworks;
+    using Scripts_Kitties;
     public class Kitties : Minigame
     {
+        public CtrTeppanPlayer player;
+        public GameObject[] Cats;
         public enum SpawnType
         {
             Straight,
-            DiagonalDown,
-            DiagonalUp,
+            DownDiagonal,
+            UpDiagonal,
+            CloseUp
         }
 
         public static Kitties instance;
@@ -63,9 +65,40 @@ namespace HeavenStudio.Games
         }
 
         public void Clap(bool isMice, bool isRightToLeft, float beat, int type = (int)SpawnType.Straight)
-        { }
+        {
+            player.ScheduleClap(beat);
+            MultiSound.Play(new MultiSound.Sound[] {
+                new MultiSound.Sound("kitties/nya1", beat),
+                new MultiSound.Sound("kitties/nya2", beat + .75f),
+                new MultiSound.Sound("kitties/nya3", beat + 1.5f)
+            });
+                BeatAction.New(Cats[0], new List<BeatAction.Action>()
+            {
+                    new BeatAction.Action(beat, delegate { Cats[0].transform.GetChild(0).gameObject.SetActive(true);}),
+                });
 
-        public void ClapClose(float beat)
-        { }
+            BeatAction.New(Cats[1], new List<BeatAction.Action>()
+            {
+                new BeatAction.Action(beat + .75f, delegate { Cats[1].transform.GetChild(0).gameObject.SetActive(true);}),
+                });
+
+            BeatAction.New(Cats[2], new List<BeatAction.Action>()
+            {
+                new BeatAction.Action(beat + 1.5f, delegate { Cats[2].transform.GetChild(0).gameObject.SetActive(true);}),
+                new BeatAction.Action(beat + 1.5f, delegate { player.canClap = true;}),
+                });
+
+        }
+
+        public void Remove(float beat)
+        {
+            BeatAction.New(Cats[0], new List<BeatAction.Action>()
+            {
+                    new BeatAction.Action(beat, delegate { Cats[0].transform.GetChild(0).gameObject.SetActive(false);}),
+                    new BeatAction.Action(beat, delegate { Cats[1].transform.GetChild(0).gameObject.SetActive(false);}),
+                    new BeatAction.Action(beat, delegate { Cats[2].transform.GetChild(0).gameObject.SetActive(false);}),
+                    new BeatAction.Action(beat, delegate { player.canClap = false;}),
+                });
+        }
     }
 }
