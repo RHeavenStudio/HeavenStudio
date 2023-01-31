40d722de0161f941d2332b8d054211c351c8c740
RaffyTaffy14 <raffytaffy0820@gmail.com>
2023-01-30 20:57:53 -0500

    End Pose Start
    Adds the End Pose cue's tweeting sound (took so long but felt so cool)

--- Assets/Scripts/Games/SumoBrothers/SumoBrothers.cs
+++ Assets/Scripts/Games/SumoBrothers/SumoBrothers.cs
@@ -12,6 +12,12 @@ namespace HeavenStudio.Games.Loaders
         {
             return new Minigame("sumoBrothers", "Sumo Brothers \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "0058CE", false, false, new List<GameAction>()
             {
+                new GameAction("endPose", "End Pose")
+                {
+                    preFunction = delegate { var e = eventCaller.currentEntity; SumoBrothers.Pose(e.beat); },
+                    defaultLength = 5f
+                }
+
             });
         }
     }
@@ -19,7 +25,7 @@ namespace HeavenStudio.Games.Loaders
 
 namespace HeavenStudio.Games
 {
-//    using Scripts_SumoBrothers;
+    //    using Scripts_SumoBrothers;
     public class SumoBrothers : Minigame
     {
         // Start is called before the first frame update
@@ -31,6 +37,30 @@ namespace HeavenStudio.Games
         // Update is called once per frame
         void Update()
         {
+
+        }
+
+        public static void Pose(float beat)
+        {
+            var cond = Conductor.instance;
+            var cueLength = 60 / cond.songBpm * 3;
+            var loopAmount = Math.Ceiling((cueLength - 0.208) / 0.395);
+            var beatsPerSecond = cond.songBpm / 60;
+            var soundBeatLength = beatsPerSecond * 0.395;
+            
+            print(cueLength + " and " + loopAmount);
+
+            List<MultiSound.Sound> sound = new List<MultiSound.Sound>();
+            sound.Add(new MultiSound.Sound("sumoBrothers/posesignalBegin", beat));
+
+            for (int i = 1; i <= loopAmount; i++)
+            {
+                sound.Add(new MultiSound.Sound("sumoBrothers/posesignalLoop", i * (float)soundBeatLength + beat - (float)soundBeatLength + ((float)beatsPerSecond * 0.208f))); 
+            }
+            print(sound.ToArray());
+            MultiSound.Play(sound.ToArray());
+
+
 
         }
     }
