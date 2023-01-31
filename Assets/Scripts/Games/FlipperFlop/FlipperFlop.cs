767c1c8c72b5fad213f9034910d52c9e512a1895
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-01-31 15:38:48 +0100

    Added animators and attention company voiceline

--- Assets/Scripts/Games/FlipperFlop/FlipperFlop.cs
+++ Assets/Scripts/Games/FlipperFlop/FlipperFlop.cs
@@ -12,6 +12,11 @@ namespace HeavenStudio.Games.Loaders
         {
             return new Minigame("flipperFlop", "Flipper-Flop \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "0058CE", false, false, new List<GameAction>()
             {
+                new GameAction("attentionCompany", "Attention Company!")
+                {
+                    preFunction = delegate {var e = eventCaller.currentEntity; FlipperFlop.AttentionCompany(e.beat); },
+                    defaultLength = 4f,
+                }
             });
         }
     }
@@ -27,6 +32,20 @@ namespace HeavenStudio.Games
         void Awake()
         {
             instance = this;
+        }
+
+        public static void AttentionCompany(float beat)
+        {
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("flipperFlop/attention/attention1", beat - 0.25f),
+                new MultiSound.Sound("flipperFlop/attention/attention2", beat),
+                new MultiSound.Sound("flipperFlop/attention/attention3", beat + 0.5f),
+                new MultiSound.Sound("flipperFlop/attention/attention4", beat + 2f),
+                new MultiSound.Sound("flipperFlop/attention/attention5", beat + 2.25f),
+                new MultiSound.Sound("flipperFlop/attention/attention6", beat + 2.5f),
+                new MultiSound.Sound("flipperFlop/attention/attention7", beat + 3),
+            }, forcePlay: true);
         }
     }
 }
