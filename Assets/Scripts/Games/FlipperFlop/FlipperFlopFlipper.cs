03466264fe1aaeeb24f9074280e9eb911a6f5b72
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-02-04 20:01:36 +0100

    Barely faces and sound

--- Assets/Scripts/Games/FlipperFlop/FlipperFlopFlipper.cs
+++ Assets/Scripts/Games/FlipperFlop/FlipperFlopFlipper.cs
@@ -9,16 +9,43 @@ namespace HeavenStudio.Games.Scripts_FlipperFlop
     {
         [SerializeField] Animator anim;
         [SerializeField] Animator faceAnim;
+        [SerializeField] GameObject leftImpact;
+        [SerializeField] GameObject rightImpact;
         public bool player;
         bool left;
         bool up;
+        bool canBlink = true;
+
+        private void Awake()
+        {
+            faceAnim.Play("FaceNormal", 0, 0);
+        }
+
+        private void Update()
+        {
+            if (UnityEngine.Random.Range(1, 600) == 1 && canBlink && !isPlaying(faceAnim, "FlipperBlink"))
+            {
+                faceAnim.DoScaledAnimationAsync("FlipperBlink", 0.5f);
+            }
+        }
 
         public void Flip(bool roll, bool hit, bool barely = false)
         {
             if (roll)
             {
-                if (player && hit && !barely) Jukebox.PlayOneShotGame("flipperFlop/roll" + (left ? "L" : "R"));
-                else if (barely) Jukebox.PlayOneShotGame("flipperFlop/tink");
+                if (player && hit && !barely)
+                {
+                    Jukebox.PlayOneShotGame("flipperFlop/roll" + (left ? "L" : "R"));
+                    faceAnim.Play("FaceNormal", 0, 0);
+                    canBlink = true;
+                }
+                else if (barely)
+                {
+                    Jukebox.PlayOneShotGame("flipperFlop/tink");
+                    faceAnim.Play("FaceBarely", 0, 0);
+                    canBlink = false;
+                }
+
                 up = !up;
             }
             else
@@ -27,16 +54,21 @@ namespace HeavenStudio.Games.Scripts_FlipperFlop
                 {
                     if (up && !barely)
                     {
+                        faceAnim.Play("FaceNormal", 0, 0);
                         Jukebox.PlayOneShotGame($"flipperFlop/flipB{UnityEngine.Random.Range(1, 3)}");
+                        canBlink = true;
                     }
                     else if (!barely)
                     {
                         Jukebox.PlayOneShotGame($"flipperFlop/flip{UnityEngine.Random.Range(1, 3)}");
-
+                        faceAnim.Play("FaceNormal", 0, 0);
+                        canBlink = true;
                     }
                     else
                     {
                         Jukebox.PlayOneShotGame("flipperFlop/tink");
+                        faceAnim.Play("FaceBarely", 0, 0);
+                        canBlink = false;
                     }
                 }
                 if (left)
@@ -54,6 +86,17 @@ namespace HeavenStudio.Games.Scripts_FlipperFlop
         public void Bop()
         {
             anim.DoScaledAnimationAsync("FlipperBop", 0.5f);
+            faceAnim.Play("FaceNormal", 0, 0);
+            canBlink = true;
+        }
+
+        bool isPlaying(Animator anim, string stateName)
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName(stateName) &&
+                    anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+                return true;
+            else
+                return false;
         }
     }
 }
