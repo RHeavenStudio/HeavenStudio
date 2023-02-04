189b4ecdca4d0a5845cb9d6fa7b8c111d0c58f4e
GhostlyGuyGD <gddpsurvaxio@gmail.com>
2023-02-04 12:49:17 +0000

    Upset and Joyful Bops
    Added 3 different types of Bops.

--- Assets/Scripts/Games/OctopusMachine/OctopusMachine.cs
+++ Assets/Scripts/Games/OctopusMachine/OctopusMachine.cs
@@ -2,6 +2,7 @@ using HeavenStudio.Util;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+// using GhostlyGuy's balls; 
 
 namespace HeavenStudio.Games.Loaders
 {
@@ -9,30 +10,24 @@ namespace HeavenStudio.Games.Loaders
     public static class NtrOctopusMachineLoader
     {
         public static Minigame AddGame(EventCaller eventCaller) {
-            return new Minigame("OctopusMachine", "Octopus Machine \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "CEU789", false, false, new List<GameAction>()
+            return new Minigame("OctopusMachine", "Octopus Machine \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "FFf362B", false, false, new List<GameAction>()
             {
 
             new GameAction("bop", "Bop")
-                    {
-                        function = delegate { var e = eventCaller.currentEntity; OctopusMachine.instance.Bop(e.beat, e["toggle"]); },
-                        parameters = new List<Param>()
-                        {
-                        new Param("toggle", false, "Reset Pose", "Resets to idle pose.")
-                        },
-                        defaultLength = 1f,
-                    },
-            });
+            {
+                function = delegate { var e = eventCaller.currentEntity; OctopusMachine.instance.Bop(e.beat, e["toggle"], e["type"]); },
+                parameters = new List<Param>()                     
+                {
+                 new Param("toggle", false, "Joyful", "Plays the animations as if you hit an input"),
+                 new Param("type", false, "Upset", "Plays the animations as if you missed."),
+                },
+                   defaultLength = 1f,
+                },
+            });     
         }
     }
 }
 
-namespace HeavenStudio.Games
-{
-    public partial class OctopusMachine : Minigame
-    {
-
-    }
-}
 
 namespace HeavenStudio.Games
 {
@@ -57,15 +52,27 @@ namespace HeavenStudio.Games
             instance = this;
         }
         
-        public void Bop(float beat, bool reset)
+        public void Bop(float beat, bool joyful, bool upset)
         {
-            
-            
-            {
-                Octopus.DoScaledAnimationAsync("Bop", 0.5f);
-                OtherOctopus.DoScaledAnimationAsync("Bop", 0.5f);
-                OctopusPlayer.DoScaledAnimationAsync("Bop", 0.5f);
-            }
+           if (joyful)
+           {
+              Octopus.DoScaledAnimationAsync("Happy", 0.5f);
+              OtherOctopus.DoScaledAnimationAsync("Happy", 0.5f);
+              OctopusPlayer.DoScaledAnimationAsync("Happy", 0.5f);
+           } 
+           else if (upset)
+           {
+              Octopus.DoScaledAnimationAsync("Angry", 0.5f);
+              OtherOctopus.DoScaledAnimationAsync("Angry", 0.5f);
+              OctopusPlayer.DoScaledAnimationAsync("Oops", 0.5f);
+           }
+           else
+           {
+              Octopus.DoScaledAnimationAsync("Bop", 0.5f);
+              OtherOctopus.DoScaledAnimationAsync("Bop", 0.5f);
+              OctopusPlayer.DoScaledAnimationAsync("Bop", 0.5f);                   
+           }
         }
     }
-}
+}               
+            
