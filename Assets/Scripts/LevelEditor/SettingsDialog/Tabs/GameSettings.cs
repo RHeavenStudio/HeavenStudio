7ba53a036f6d8c153ff8fc993b4d5db865ed1785
minenice55 <star.elementa@gmail.com>
2023-02-05 15:11:06 -0500

    add game settings script

---
+++ Assets/Scripts/LevelEditor/SettingsDialog/Tabs/GameSettings.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+using HeavenStudio.Common;
+
+namespace HeavenStudio.Editor
+{
+    public class GameSettings : TabsContent
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void OnOpenTab()
+        {
+        }
+
+        public override void OnCloseTab()
+        {
+        }
+    }
+}
