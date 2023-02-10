6a43c6f7a1c2dc35cd33ad0d6ac712041c83976e
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-10 02:59:36 -0500

    Start

---
+++ Assets/Scripts/NewEditor/Editor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace HeavenStudio.LevelEditor
+{
+    public class Editor : MonoBehaviour
+    {
+
+    }
+}
