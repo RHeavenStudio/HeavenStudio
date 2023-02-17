ae87b91c8b7203a8bac7935f77675103dc60b84a
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-02-17 18:42:46 +0100

    Tapping math stuff done

--- Assets/Scripts/Games/TapTroupe/TapTroupe.cs
+++ Assets/Scripts/Games/TapTroupe/TapTroupe.cs
@@ -1,5 +1,6 @@
 using HeavenStudio.Util;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -20,6 +21,16 @@ namespace HeavenStudio.Games.Loaders
                     parameters = new List<Param>()
                     {
                         new Param("startTap", false, "Start Tap Voice Line", "Whether or not it should say -Tap!- on the first step.")
+                    }
+                },
+                new GameAction("tapping", "Tapping")
+                {
+                    preFunction = delegate { var e = eventCaller.currentEntity; TapTroupe.PreTapping(e.beat, e.length, e["okay"]); },
+                    defaultLength = 3f,
+                    resizable = true,
+                    parameters = new List<Param>()
+                    {
+                        new Param("okay", true, "Okay Voice Line", "Whether or not the tappers should say -Okay!- after successfully tapping.")
                     }
                 },
                 new GameAction("bop", "Bop")
@@ -44,12 +55,18 @@ namespace HeavenStudio.Games
         [SerializeField] List<TapTroupeCorner> npcCorners = new List<TapTroupeCorner>();
         [Header("Properties")]
         private static List<QueuedSteps> queuedSteps = new List<QueuedSteps>();
+        private static List<QueuedTaps> queuedTaps = new List<QueuedTaps>();
         public struct QueuedSteps
         {
             public float beat;
             public float length;
             public bool startTap;
-
+        }
+        public struct QueuedTaps
+        {
+            public float beat;
+            public float length;
+            public bool okay;
         }
         private int stepSound = 1;
 
@@ -58,6 +75,7 @@ namespace HeavenStudio.Games
         void OnDestroy()
         {
             if (queuedSteps.Count > 0) queuedSteps.Clear();
+            if (queuedTaps.Count > 0) queuedTaps.Clear();
         }
 
         void Awake()
@@ -77,6 +95,14 @@ namespace HeavenStudio.Games
                         Stepping(step.beat, step.length, step.startTap);
                     }
                     queuedSteps.Clear();
+                }
+                if (queuedTaps.Count > 0)
+                {
+                    foreach (var tap in queuedTaps)
+                    {
+                        Tapping(tap.beat, tap.length, tap.okay);
+                    }
+                    queuedTaps.Clear();
                 }
             }
         }
@@ -117,6 +143,60 @@ namespace HeavenStudio.Games
                 }),
                 new BeatAction.Action(beat, delegate { if (startTap) Jukebox.PlayOneShotGame("tapTroupe/startTap"); })
             });
+        }
+
+        public static void PreTapping(float beat, float length, bool okay)
+        {
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("tapTroupe/tapReady1", beat - 2f),
+                new MultiSound.Sound("tapTroupe/tapReady2", beat - 1f),
+            }, forcePlay: true);
+            if (GameManager.instance.currentGame == "tapTroupe")
+            {
+                TapTroupe.instance.Tapping(beat, length, okay);
+            }
+            else
+            {
+                queuedTaps.Add(new QueuedTaps { beat = beat, length = length, okay = okay });
+            }
+        }
+
+        public void Tapping(float beat, float length, bool okay)
+        {
+            float actualLength = length - 0.5f;
+            actualLength -= actualLength % 0.75f;
+            bool secondBam = false;
+            if (actualLength < 2.25f) actualLength = 2.25f;
+            List<MultiSound.Sound> soundsToPlay = new List<MultiSound.Sound>
+            {
+                new MultiSound.Sound("tapTroupe/tapAnd", beat)
+            };
+            for (float i = 0; i < actualLength; i += 0.75f)
+            {
+                string soundToPlay = "bamvoice1";
+                float beatToSpawn = beat + i + 0.5f;
+                if (i + 0.75f >= actualLength)
+                {
+                    soundToPlay = "startTap";
+                    beatToSpawn = Mathf.Ceil(beat + i);
+                }
+                else if (i + 1.5f >= actualLength)
+                {
+                    soundToPlay = "tapvoice2";
+                }
+                else if (i + 2.25f >= actualLength)
+                {
+                    soundToPlay = "tapvoice1";
+                }
+                else if (secondBam)
+                {
+                    soundToPlay = "bamvoice2";
+                }
+                soundsToPlay.Add(new MultiSound.Sound($"tapTroupe/{soundToPlay}", beatToSpawn));
+                secondBam = !secondBam;
+            }
+            MultiSound.Play(soundsToPlay.ToArray(), forcePlay: true);
         }
 
         public void Bop()
