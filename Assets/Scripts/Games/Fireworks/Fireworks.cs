811cbafa6f44f1d5a9f5a7bf2940e9c05357817e
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-02-20 13:55:03 +0100

    Started

--- Assets/Scripts/Games/Fireworks/Fireworks.cs
+++ Assets/Scripts/Games/Fireworks/Fireworks.cs
@@ -11,6 +11,24 @@ namespace HeavenStudio.Games.Loaders
         public static Minigame AddGame(EventCaller eventCaller) {
             return new Minigame("fireworks", "Fireworks \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "0058CE", false, false, new List<GameAction>()
             {
+                new GameAction("firework", "Firework")
+                {
+                    function = delegate {var e = eventCaller.currentEntity; Fireworks.instance.SpawnFirework(e.beat, false, e["whereToSpawn"]); },
+                    defaultLength = 4f,
+                    parameters = new List<Param>()
+                    {
+                        new Param("whereToSpawn", Fireworks.WhereToSpawn.Middle, "Where to spawn?", "Where should the firework spawn?")
+                    }
+                },
+                new GameAction("sparkler", "Sparkler")
+                {
+                    function = delegate {var e = eventCaller.currentEntity; Fireworks.instance.SpawnFirework(e.beat, true, e["whereToSpawn"]); },
+                    defaultLength = 2f,
+                    parameters = new List<Param>()
+                    {
+                        new Param("whereToSpawn", Fireworks.WhereToSpawn.Middle, "Where to spawn?", "Where should the firework spawn?")
+                    }
+                }
             });
         }
     }
@@ -21,16 +39,43 @@ namespace HeavenStudio.Games
     using Scripts_Fireworks;
     public class Fireworks : Minigame
     {
-        // Start is called before the first frame update
+        public enum WhereToSpawn
+        {
+            Left = 0,
+            Right = 1,
+            Middle = 2
+        }
+        [Header("Components")]
+        [SerializeField] Transform spawnLeft;
+        [SerializeField] Transform spawnRight;
+        [SerializeField] Transform spawnMiddle;
+        [SerializeField] Rocket firework;
+
+        public static Fireworks instance;
+
         void Awake()
         {
-            
+            instance = this;
         }
 
-        // Update is called once per frame
-        void Update()
+        public void SpawnFirework(float beat, bool isSparkler, int whereToSpawn)
         {
-            
+            Transform spawnPoint = spawnMiddle;
+            switch (whereToSpawn)
+            {
+                case (int)WhereToSpawn.Left:
+                    spawnPoint = spawnLeft;
+                    break;
+                case (int)WhereToSpawn.Right:
+                    spawnPoint = spawnRight;
+                    break;
+                default:
+                    spawnPoint = spawnMiddle;
+                    break;
+            }
+            Rocket spawnedRocket = Instantiate(firework, spawnPoint, false);
+            spawnedRocket.isSparkler = isSparkler;
+            spawnedRocket.Init(beat);
         }
     }
 }
--- Assets/Scripts/Games/Fireworks/Rocket.cs
+++ Assets/Scripts/Games/Fireworks/Rocket.cs
@@ -8,18 +8,43 @@ namespace HeavenStudio.Games.Scripts_Fireworks
 {
     public class Rocket : PlayerActionObject
     {
+        [SerializeField] ParticleSystem particleEffect;
         public bool isSparkler;
+        private Fireworks game;
 
-        // Start is called before the first frame update
         void Awake()
         {
-            
+            game = Fireworks.instance;
         }
 
-        // Update is called once per frame
+        public void Init(float beat)
+        {
+            if (isSparkler) Jukebox.PlayOneShotGame("fireworks/sparkler");
+            else Jukebox.PlayOneShotGame("fireworks/rocket");
+            game.ScheduleInput(beat, isSparkler ? 1f : 3f, InputType.STANDARD_DOWN, Just, Out, Out);
+        }
+
         void Update()
         {
-            
+            transform.position = new Vector3(transform.position.x, transform.position.y + (isSparkler ? 0.05f : 0.01f), transform.position.z);
         }
+
+        void Just(PlayerActionEvent caller, float state)
+        {
+            if (state >= 1f || state <= -1f)
+            {
+                return;
+            }
+            Success();
+        }
+
+        void Success()
+        {
+            Jukebox.PlayOneShotGame("fireworks/explodeRocket");
+            Instantiate(particleEffect.gameObject, transform, false);
+            Destroy(gameObject);
+        }
+
+        void Out(PlayerActionEvent caller) { }
     }
 }
