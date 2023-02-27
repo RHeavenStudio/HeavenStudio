3e966a054f21b6163f52c70854f90c1e56801c25
ThatZeoMan <67521686+ThatZeoMan@users.noreply.github.com>
2023-02-26 21:44:40 -0600

    Start to redo export ui

--- Assets/Scripts/AppInfo.cs
+++ Assets/Scripts/AppInfo.cs
@@ -1,8 +1,8 @@
 using System;
 
 public static class AppInfo {
-    public const string Version = "0.0.967";
-    public static readonly DateTime Date = new DateTime(2023, 02, 05, 02, 49, 33, 317, DateTimeKind.Utc);
+    public const string Version = "0.0.968";
+    public static readonly DateTime Date = new DateTime(2023, 02, 27, 03, 36, 17, 741, DateTimeKind.Utc);
 }
 
 
