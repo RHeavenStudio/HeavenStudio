00e0017fb37dd3387d65bab7a7ecd367c84af021
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-02-28 11:21:34 +0100

    Got Started on the inputs

--- Assets/Scripts/Games/TheDazzles/TheDazzles.cs
+++ Assets/Scripts/Games/TheDazzles/TheDazzles.cs
@@ -12,7 +12,16 @@ namespace HeavenStudio.Games.Loaders
         {
             return new Minigame("theDazzles", "The Dazzles", "E7A59C", false, false, new List<GameAction>()
             {
-
+                new GameAction("crouch", "Crouch")
+                {
+                    function = delegate { TheDazzles.instance.Crouch(eventCaller.currentEntity.beat); },
+                    defaultLength = 3f,
+                },
+                new GameAction("poseThree", "Pose Horizontal")
+                {
+                    function = delegate { TheDazzles.instance.PoseThree(eventCaller.currentEntity.beat); },
+                    defaultLength = 3f
+                }
             });
         }
     }
@@ -24,10 +33,135 @@ namespace HeavenStudio.Games
     {
         public static TheDazzles instance;
 
+        [Header("Components")]
+        [SerializeField] List<TheDazzlesGirl> npcGirls = new List<TheDazzlesGirl>();
+        [SerializeField] TheDazzlesGirl player;
+        [SerializeField] ParticleSystem poseEffect;
+        [SerializeField] ParticleSystem starsEffect;
+
         void Awake()
         {
             instance = this;
         }
+
+        void Update()
+        {
+            var cond = Conductor.instance;
+
+            if (cond.isPlaying && !cond.isPaused)
+            {
+                if (PlayerInput.Pressed() && !IsExpectingInputNow(InputType.STANDARD_DOWN))
+                {
+                    player.Prepare(false);
+                }
+                if (PlayerInput.PressedUp() && !IsExpectingInputNow(InputType.STANDARD_UP))
+                {
+                    player.UnPrepare();
+                }
+            }
+        }
+
+        public void Crouch(float beat)
+        {
+            ScheduleInput(beat, 2f, InputType.STANDARD_DOWN, JustCrouch, MissCrouch, Nothing);
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("theDazzles/hold3", beat),
+                new MultiSound.Sound("theDazzles/hold2", beat + 1f),
+                new MultiSound.Sound("theDazzles/hold1", beat + 2f),
+            }, forcePlay: true);
+
+            BeatAction.New(instance.gameObject, new List<BeatAction.Action>()
+            {
+                new BeatAction.Action(beat, delegate
+                {
+                    npcGirls[1].Prepare();
+                    npcGirls[4].Prepare();
+                }),
+                new BeatAction.Action(beat + 1f, delegate
+                {
+                    npcGirls[0].Prepare();
+                    npcGirls[3].Prepare();
+                }),
+                new BeatAction.Action(beat + 2f, delegate
+                {
+                    npcGirls[2].Prepare();
+                }),
+            });
+        }
+
+        public void PoseThree(float beat)
+        {
+            ScheduleInput(beat, 2f, InputType.STANDARD_UP, JustPose, MissPose, Nothing);
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("theDazzles/posePartner", beat),
+                new MultiSound.Sound("theDazzles/posePartner", beat + 1f),
+            }, forcePlay: true);
+
+            BeatAction.New(instance.gameObject, new List<BeatAction.Action>()
+            {
+                new BeatAction.Action(beat, delegate
+                {
+                    npcGirls[1].Pose();
+                    npcGirls[4].Pose();
+                }),
+                new BeatAction.Action(beat + 1f, delegate
+                {
+                    npcGirls[0].Pose();
+                    npcGirls[3].Pose();
+                }),
+                new BeatAction.Action(beat + 2f, delegate
+                {
+                    npcGirls[2].Pose();
+                }),
+            });
+        }
+
+        void JustCrouch(PlayerActionEvent caller, float state)
+        {
+            if (state >= 1f || state <= -1f)
+            {
+                return;
+            }
+            SuccessCrouch();
+        }
+
+        void SuccessCrouch()
+        {
+            player.Prepare();
+            Jukebox.PlayOneShotGame("theDazzles/crouch");
+        }
+
+        void MissCrouch(PlayerActionEvent caller)
+        {
+
+        }
+
+        void JustPose(PlayerActionEvent caller, float state)
+        {
+            if (state >= 1f || state <= -1f)
+            {
+                player.Pose(false);
+                return;
+            }
+            SuccessPose(false);
+        }
+
+        void SuccessPose(bool stars)
+        {
+            player.Pose();
+            poseEffect.Play();
+            Jukebox.PlayOneShotGame("theDazzles/posePlayer");
+            if (stars) starsEffect.Play();
+        }
+
+        void MissPose(PlayerActionEvent caller)
+        {
+
+        }
+
+        void Nothing(PlayerActionEvent caller) { }
     }
 }
 
--- Assets/Scripts/Games/TheDazzles/TheDazzlesGirl.cs
+++ Assets/Scripts/Games/TheDazzles/TheDazzlesGirl.cs
@@ -7,7 +7,55 @@ namespace HeavenStudio.Games.Scripts_TheDazzles
 {
     public class TheDazzlesGirl : MonoBehaviour
     {
+        enum Emotion
+        {
+            Neutral = 0,
+            Happy = 1,
+            Angry = 2
+        }
+        Emotion currentEmotion;
+        Animator anim;
+        [SerializeField] Animator holdEffectAnim;
+        [SerializeField] SpriteRenderer headSprite;
+        [SerializeField] SpriteRenderer mouthSprite;
+        TheDazzles game;
 
+        void Awake()
+        {
+            anim = GetComponent<Animator>();
+            game = TheDazzles.instance;
+        }
+
+        public void Prepare(bool hit = true)
+        {
+            anim.Play("Prepare", 0, 0);
+        }
+
+        public void Pose(bool hit = true)
+        {
+            anim.DoScaledAnimationAsync("Pose", 0.5f);
+        }
+
+        public void UnPrepare()
+        {
+            game.ScoreMiss(1f);
+            anim.Play("Prepare", 0, 0);
+        }
+
+        public void Bop()
+        {
+            switch (currentEmotion)
+            {
+                case Emotion.Neutral:
+                    anim.DoScaledAnimationAsync("IdleBop", 0.5f);
+                    break;
+                case Emotion.Happy:
+                    anim.DoScaledAnimationAsync("HappyBop", 0.5f);
+                    break;
+                case Emotion.Angry:
+                    break;
+            }
+        }
     }
 }
 
