2e0c0b8b051dd879154e3784a50bff9d1fa7117d
RaffyTaffy14 <raffytaffy0820@gmail.com>
2023-03-04 13:49:14 -0500

    Imported Upscaled Sumou_BG
    Higher resolution background and Inu Sensei sprites.

--- Assets/Scripts/Games/SumoBrothers/SumoBrothers.cs
+++ Assets/Scripts/Games/SumoBrothers/SumoBrothers.cs
@@ -10,26 +10,26 @@ namespace HeavenStudio.Games.Loaders
     {
         public static Minigame AddGame(EventCaller eventCaller)
         {
-            return new Minigame("sumoBrothers", "Sumo Brothers \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "0058CE", false, false, new List<GameAction>()
+            return new Minigame("sumoBrothers", "Sumo Brothers \n<color=#eb5454>[INITIALIZATION ONLY]</color>", "EDED15", false, false, new List<GameAction>()
             {
                  new GameAction("bop", "Bop")
                 {
-                    function = delegate { var e = eventCaller.currentEntity; SumoBrothers.instance.Bop(e["toggle"], e["toggle1"]); },
+                    function = delegate { var e = eventCaller.currentEntity; SumoBrothers.instance.Bop(e["inu"], e["sumo"]); },
                     parameters = new List<Param>()
                     {
-                        new Param("toggle", true, "Inu Bop", "Whether Inu Sensei bops or not."),
-                        new Param("toggle1", true, "Brothers Bop", "Whether the Sumo Brothers bop or not."),
+                        new Param("inu", true, "Inu Bop", "Whether Inu Sensei bops or not."),
+                        new Param("sumo", true, "Brothers Bop", "Whether the Sumo Brothers bop or not."),
                     },
                     defaultLength = 0.5f
                 },
 
                  new GameAction("crouch", "Crouch")
                 {
-                    function = delegate { var e = eventCaller.currentEntity; SumoBrothers.instance.Crouch(e.beat, e.length, e["toggle"], e["toggle1"]); },
+                    function = delegate { var e = eventCaller.currentEntity; SumoBrothers.instance.Crouch(e.beat, e.length, e["inu"], e["sumo"]); },
                     parameters = new List<Param>()
                     {
-                        new Param("toggle", true, "Inu Crouch", "Whether Inu Sensei crouches or not."),
-                        new Param("toggle1", true, "Brothers Crouch", "Whether the Sumo Brothers crouch or not.")
+                        new Param("inu", true, "Inu Crouch", "Whether Inu Sensei crouches or not."),
+                        new Param("sumo", true, "Brothers Crouch", "Whether the Sumo Brothers crouch or not.")
                     },
                     defaultLength = 1f,
                     resizable = true
@@ -37,7 +37,12 @@ namespace HeavenStudio.Games.Loaders
 
                 new GameAction("endPose", "End Pose")
                 {
-                    function = delegate { var e = eventCaller.currentEntity; SumoBrothers.instance.Pose(e.beat); },
+                    function = delegate { var e = eventCaller.currentEntity; SumoBrothers.instance.EndPose(e.beat); },
+                    parameters = new List<Param>()
+                    {
+                        new Param("type", SumoBrothers.PoseType.Crouching, "Pose", "The pose that the Sumo Brothers will make."),
+                        new Param("bg", SumoBrothers.BGType.TheGreatWave, "Background", "The background that appears on a successful input.")
+                    },
                     defaultLength = 5f
                 }
 
@@ -57,13 +62,38 @@ namespace HeavenStudio.Games
         [SerializeField] Animator sumoBrotherG;
 
         [Header("Properties")]
-        public bool goBopSumo;
-        public bool goBopInu;
-        public bool allowBopSumo;
-        public bool allowBopInu;
+        static List<queuedSumoInputs> queuedInputs = new List<queuedSumoInputs>();
+        public struct queuedSumoInputs
+        {
+            public float beat;
+            public int cue;
+            public int poseType;
+            public int poseBG;
+        }
+
+        private bool goBopSumo;
+        private bool goBopInu;
+
+        private bool allowBopSumo;
+        private bool allowBopInu;
+
         private float lastReportedBeat = 0f;
 
         public static SumoBrothers instance;
+
+        public enum PoseType
+        {
+            Crouching = 0,
+            Crossed = 1,
+            Pointing = 2,
+            Finale = 3
+        }
+
+        public enum BGType
+        {
+            TheGreatWave = 0,
+            OtaniOniji = 1
+        }
 
         // Start is called before the first frame update
         void Awake()
@@ -74,6 +104,14 @@ namespace HeavenStudio.Games
             allowBopSumo = true;
             instance = this;
 
+        }
+
+        void OnDestroy()
+        {
+            if (!Conductor.instance.isPlaying || Conductor.instance.isPaused)
+            {
+                if (queuedInputs.Count > 0) queuedInputs.Clear();
+            }
         }
 
         // Update is called once per frame
@@ -93,6 +131,11 @@ namespace HeavenStudio.Games
                     sumoBrotherP.DoScaledAnimationAsync("SumoBop", 0.5f);
                     sumoBrotherG.DoScaledAnimationAsync("SumoBop", 0.5f);
                 }
+
+            }
+
+            if (PlayerInput.Pressed() && !IsExpectingInputNow())
+            {
 
             }
         }
@@ -131,39 +174,66 @@ namespace HeavenStudio.Games
 
         }
 
-        public void Pose(float beat)
+        public void EndPose(float beat)
         {
             var cond = Conductor.instance;
-            var cueLength = 3 * cond.secPerBeat;
-            print(cond.pitchedSecPerBeat);
-            var loopAmount = Math.Round((cueLength - 0.208) / 0.395);
-            var beatsPerSecond = cond.songBpm / 60;
-            var soundBeatLength = beatsPerSecond * 0.395;
+            #region Tweet
+                var loopAmount = Math.Round((3 * cond.secPerBeat - 0.208) / 0.395);
+                var beatsPerSecond = cond.songBpm / 60;
+                var soundBeatLength = beatsPerSecond * 0.395;
+
+                List<MultiSound.Sound> sound = new List<MultiSound.Sound>();
+                sound.Add(new MultiSound.Sound("sumoBrothers/posesignalBegin", beat));
+
+                for (int i = 1; i <= loopAmount; i++)
+                {
+                    sound.Add(new MultiSound.Sound("sumoBrothers/posesignalLoop", i * (float)soundBeatLength + beat - (float)soundBeatLength + ((float)beatsPerSecond * 0.208f))); 
+                }
+
+                MultiSound.Play(sound.ToArray());
+
+                var tweetLength = (float)loopAmount * (float)soundBeatLength - (float)soundBeatLength + ((float)beatsPerSecond * 0.208f);
+                print(tweetLength);
+                
+            #endregion
+
+            //SumoBrothers.instance.EndPoseCheck(beat);
+
             
-
-            List<MultiSound.Sound> sound = new List<MultiSound.Sound>();
-            sound.Add(new MultiSound.Sound("sumoBrothers/posesignalBegin", beat));
-
-            for (int i = 1; i <= loopAmount; i++)
-            {
-                sound.Add(new MultiSound.Sound("sumoBrothers/posesignalLoop", i * (float)soundBeatLength + beat - (float)soundBeatLength + ((float)beatsPerSecond * 0.208f))); 
-            }
-
-            MultiSound.Play(sound.ToArray());
 
             allowBopInu = false;
             inuSensei.DoScaledAnimationAsync("InuTweet", 0.5f);
-            var tweetLength = (float)loopAmount * (float)soundBeatLength - (float)soundBeatLength + ((float)beatsPerSecond * 0.208f);
-            print(tweetLength);
-            BeatAction.New(instance.gameObject, new List<BeatAction.Action>() { 
-                new BeatAction.Action(beat + tweetLength + 1, delegate { allowBopInu = true; }),
-                new BeatAction.Action(beat + tweetLength + 1, delegate { if (goBopInu == false) inuSensei.DoScaledAnimationAsync("InuIdle", 0.5f); }),
-                new BeatAction.Action(beat + tweetLength + 1, delegate { if (goBopInu == true) inuSensei.DoScaledAnimationAsync("InuBop", 0.5f); })
+
+            BeatAction.New(instance.gameObject, new List<BeatAction.Action>() {
+                new BeatAction.Action(beat + 3, delegate { allowBopInu = true; }),
+                new BeatAction.Action(beat + 3, delegate { if (goBopInu == false) inuSensei.DoScaledAnimationAsync("InuIdle", 0.5f); }),
+                new BeatAction.Action(beat + 3, delegate { if (goBopInu == true) inuSensei.DoScaledAnimationAsync("InuBop", 0.5f); })
             });
 
-            
+        }
+
+        void EndPoseCheck(float beat)
+        {
+
+            //ScheduleInput(beat, 5f, InputType.STANDARD_ALT_DOWN, SuccessEndPose, MissEndPose, Nothing);
+
+        }
+
+        void SuccessEndPose(PlayerActionEvent caller)
+        {
+
 
 
         }
+
+        void MissEndPose(PlayerActionEvent caller)
+        {
+
+
+
+        }
+
+        void Nothing(PlayerActionEvent caller) { }
+
     }
 }
