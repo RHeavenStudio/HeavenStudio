0fe4c81fd44b1dd481c083ca71054e1ea53c95c8
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-03-07 15:29:09 +0100

    Fixed drumming practice stuff

--- Assets/Scripts/Games/AirRally/AirRally.cs
+++ Assets/Scripts/Games/AirRally/AirRally.cs
@@ -294,7 +294,7 @@ namespace HeavenStudio.Games
             }
             else
             {
-                tweenForForth = Forthington.gameObject.transform.DOMoveZ(wayPointZForForth, .7f).SetEase(Ease.InOutQuad);
+                tweenForForth = Forthington.gameObject.transform.DOMoveZ(wayPointZForForth, .7f).SetEase(Ease.OutQuad);
             }
         }
 
