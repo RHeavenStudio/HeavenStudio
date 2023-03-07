fa818c542477e9258559bca5f42ccc5810b8b2bc
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-03-07 17:54:04 +0100

    That should be all of them except space dance and dj school

--- Assets/Scripts/Games/TrickClass/TrickClass.cs
+++ Assets/Scripts/Games/TrickClass/TrickClass.cs
@@ -31,11 +31,15 @@ namespace HeavenStudio.Games.Loaders
                     },
                     defaultLength = 3,
                 },
-                new GameAction("bop", "")
+                new GameAction("bop", "Bop")
                 {
-                    function = delegate { var e = eventCaller.currentEntity; TrickClass.instance.Bop(e.beat, e.length); },
+                    function = delegate { var e = eventCaller.currentEntity; TrickClass.instance.Bop(e.beat, e.length, e["bop"], e["autoBop"]); },
                     resizable = true, 
-                    hidden = true
+                    parameters = new List<Param>()
+                    {
+                        new Param("bop", true, "Bop", "Should the girl and boy bop?"),
+                        new Param("autoBop", false, "Bop (Auto)", "Should the girl and boy auto bop?")
+                    }
                 },
             });
         }
@@ -82,6 +86,7 @@ namespace HeavenStudio.Games
 
         public static TrickClass instance;
         public GameEvent bop = new GameEvent();
+        bool goBop = true;
 
         public float playerCanDodge = Single.MinValue;
         float playerBopStart = Single.MinValue;
@@ -101,7 +106,7 @@ namespace HeavenStudio.Games
         private void Update()
         {
             var cond = Conductor.instance;
-            if (cond.ReportBeat(ref bop.lastReportedBeat, bop.startBeat % 1))
+            if (cond.ReportBeat(ref bop.lastReportedBeat, bop.startBeat % 1) && goBop)
             {
                 if (cond.songPositionInBeats > playerBopStart)
                     playerAnim.DoScaledAnimationAsync("Bop");
@@ -148,10 +153,27 @@ namespace HeavenStudio.Games
             }
         }
 
-        public void Bop(float beat, float length)
+        public void Bop(float beat, float length, bool shouldBop, bool autoBop)
         {
-            bop.startBeat = beat;
-            bop.length = length;
+            var cond = Conductor.instance;
+            goBop = autoBop;
+            if (shouldBop)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    BeatAction.New(instance.gameObject, new List<BeatAction.Action>()
+                    {
+                        new BeatAction.Action(beat + i, delegate
+                        {
+                            if (cond.songPositionInBeats > playerBopStart)
+                                playerAnim.DoScaledAnimationAsync("Bop");
+
+                            if (cond.songPositionInBeats > girlBopStart)
+                                girlAnim.DoScaledAnimationAsync("Bop");
+                        })
+                    });
+                }
+            }
         }
 
         public static void PreTossObject(float beat, int type)
