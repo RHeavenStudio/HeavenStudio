b7ca65fa7237a559a00da0d4e29aa1b115c5c1e1
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-04-19 17:50:31 +0200

    Basic pass ball implemented

--- Assets/Scripts/Games/TossBoys/TossBoys.cs
+++ Assets/Scripts/Games/TossBoys/TossBoys.cs
@@ -20,6 +20,14 @@ namespace HeavenStudio.Games.Loaders
                     {
                         new Param("who", TossBoys.KidChoice.Akachan, "Receiver", "Who will receive the ball?")
                     }
+                },
+                new GameAction("pass", "Pass Ball")
+                {
+                    defaultLength = 2f,
+                    parameters = new List<Param>()
+                    {
+                        new Param("who", TossBoys.KidChoice.Akachan, "Receiver", "Who will receive the ball?")
+                    }
                 }
             });
         }
@@ -28,7 +36,6 @@ namespace HeavenStudio.Games.Loaders
 namespace HeavenStudio.Games
 {
     using Scripts_TossBoys;
-    using UnityEngine.UI.Extensions;
 
     public class TossBoys : Minigame
     {
@@ -56,7 +63,7 @@ namespace HeavenStudio.Games
         WhichTossKid lastReceiver = WhichTossKid.None;
         WhichTossKid currentReceiver = WhichTossKid.None;
         public TossBoysBall currentBall = null;
-
+        Dictionary<float, WhichTossKid> passBallDict = new Dictionary<float, WhichTossKid>();
         public static TossBoys instance;
 
         private void Awake()
@@ -87,11 +94,68 @@ namespace HeavenStudio.Games
         public void Dispense(float beat, int who)
         {
             if (currentBall != null) return;
+            SetPassBallEvents();
             SetReceiver(who);
             GetCurrentReceiver().ShowArrow(beat, 1f);
             Jukebox.PlayOneShotGame("tossBoys/ballStart");
             hatchAnim.Play("HatchOpen");
             currentBall = Instantiate(ballPrefab, transform);
+            ScheduleInput(beat, 2f, GetInputTypeBasedOnCurrentReceiver(), JustHitBall, Miss, Empty);
+        }
+
+        void SetPassBallEvents()
+        {
+            passBallDict.Clear();
+            var passBallEvents = EventCaller.GetAllInGameManagerList("tossBoys", new string[] { "pass" });
+            for (int i = 0;  i < passBallEvents.Count; i++)
+            {
+                if (passBallEvents[i].beat >= Conductor.instance.songPositionInBeats)
+                {
+                    if (passBallDict.ContainsKey(passBallEvents[i].beat)) continue;
+                    passBallDict.Add(passBallEvents[i].beat, (WhichTossKid)passBallEvents[i]["who"]);
+                }
+            }
+        }
+
+        void DeterminePass(float beat)
+        {
+            var tempLastReceiver = lastReceiver;
+            lastReceiver = currentReceiver;
+            if (passBallDict.TryGetValue(beat, out var receiver))
+            {
+                currentReceiver = receiver;
+            }
+            else
+            {
+                currentReceiver = tempLastReceiver;
+            }
+            PassBall(beat);
+        }
+
+        void PassBall(float beat)
+        {
+            string last = GetColorBasedOnTossKid(lastReceiver, false);
+            string current = GetColorBasedOnTossKid(currentReceiver, true);
+            float secondBeat = 1f;
+            switch (last + current)
+            {
+                case "blueRed":
+                case "yellowRed":
+                case "redYellow":
+                    secondBeat = 0.5f;
+                    break;
+                default:
+                    secondBeat = 1f;
+                    break;
+            }
+            Debug.Log("SecondBeat: " + secondBeat + "\n1st: " + last + current + 1 + "\n2nd: " + last + current + 2 + "\n3rd: " + last + current + 3);
+            List<MultiSound.Sound> soundsToPlay = new List<MultiSound.Sound>()
+            {
+                new MultiSound.Sound("tossBoys/" + last + current + 1, beat),
+                new MultiSound.Sound("tossBoys/" + last + current + 2, beat + secondBeat),
+            };
+            if (secondBeat == 0.5f) soundsToPlay.Add(new MultiSound.Sound("tossBoys/" + last + current + 3, beat + 1));
+            MultiSound.Play(soundsToPlay.ToArray());
             ScheduleInput(beat, 2f, GetInputTypeBasedOnCurrentReceiver(), JustHitBall, Miss, Empty);
         }
 
@@ -101,10 +165,11 @@ namespace HeavenStudio.Games
             if (state >= 1f || state <= -1f)
             {
                 GetCurrentReceiver().Barely();
+                DeterminePass(caller.timer + caller.startBeat);
                 return;
             }
             GetCurrentReceiver().HitBall();
-            lastReceiver = currentReceiver;
+            DeterminePass(caller.timer + caller.startBeat);
         }
 
         void Miss(PlayerActionEvent caller)
@@ -130,6 +195,21 @@ namespace HeavenStudio.Games
                     return kiiyan;
                 default:
                     return null;
+            }
+        }
+
+        string GetColorBasedOnTossKid(WhichTossKid tossKid, bool capital)
+        {
+            switch (tossKid)
+            {
+                case WhichTossKid.Akachan:
+                    return capital ? "Red" : "red";
+                case WhichTossKid.Aokun:
+                    return capital ? "Blue" : "blue";
+                case WhichTossKid.Kiiyan:
+                    return capital ? "Yellow" : "yellow";
+                default:
+                    return "";
             }
         }
 
