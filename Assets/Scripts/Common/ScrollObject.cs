f446ca9ffccb7e2abc7450901b9cb599e4fc09ee
AstrlJelly <bdlawson115@gmail.com>
2023-05-17 16:28:48 -0400

    a few things + new icon!!

--- Assets/Scripts/Common/ScrollObject.cs
+++ Assets/Scripts/Common/ScrollObject.cs
@@ -13,37 +13,27 @@ namespace HeavenStudio.Common
 
         public void LateUpdate()
         {
+            var pos = gameObject.transform.position;
             float songPos = Conductor.instance.GetBpmAtBeat(Conductor.instance.songPosition)/10000;
             float _x = songPos*XSpeed;
             float _y = songPos*YSpeed;
             if (AutoScroll && Conductor.instance.NotStopped()) {
                 gameObject.transform.position += new Vector3(_x, _y);
-                
-                if (XSpeed != 0) {
-                    if (XSpeed > 0) {
-                        if (gameObject.transform.position.x >= PositiveBounds.x) {
-                            SetPos(NegativeBounds.x, gameObject.transform.position.y);
-                        }
-                    } else {
-                        if (gameObject.transform.position.x <= NegativeBounds.x) {
-                            SetPos(PositiveBounds.x, gameObject.transform.position.y);
-                        }
-                    }
+
+                if (XSpeed > 0 && pos.x >= PositiveBounds.x) {
+                    SetPos(NegativeBounds.x, pos.y);
                 }
-                
-                if (YSpeed != 0) {
-                    if (YSpeed > 0) {
-                        if (gameObject.transform.position.y >= PositiveBounds.y) {
-                            SetPos(gameObject.transform.position.x, NegativeBounds.y);
-                        }
-                    } else {
-                        if (gameObject.transform.position.y >= NegativeBounds.y) {
-                            SetPos(gameObject.transform.position.x, PositiveBounds.y);
-                        }
-                    }
-                }    
+                if (XSpeed < 0 && pos.x <= NegativeBounds.x) {
+                    SetPos(PositiveBounds.x, pos.y);
+                }
+
+                if (YSpeed > 0 && pos.y >= PositiveBounds.y) {
+                    SetPos(pos.x, NegativeBounds.y);
+                }
+                if (YSpeed < 0 && pos.y <= NegativeBounds.y) {
+                    SetPos(pos.x, PositiveBounds.y);
+                }
             }
-            Debug.Log(gameObject.transform.position);
         }
 
         public void SetPos(float x, float y)
