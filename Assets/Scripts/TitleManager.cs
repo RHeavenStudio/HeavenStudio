8e35e4280e6878c879913f85756ec0c07452bfb8
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-05-19 00:25:57 +0200

    The logo now bops to the beat

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -10,10 +10,52 @@ namespace HeavenStudio
     {
         [SerializeField] private Button createButton;
 
-        [SerializeField] private float Bpm = 114f;
+        [SerializeField] private Animator logoAnim;
+
+        [SerializeField] private float bpm = 114f;
+
+        private AudioSource musicSource;
+
+        private double songPosBeat;
+        private double songPos;
+        private double time;
+        private double targetBopBeat;
+
+        private double lastAbsTime;
         private void Start()
         {
+            musicSource = GetComponent<AudioSource>();
             createButton.onClick.AddListener(delegate { GlobalGameManager.LoadScene("Editor"); Jukebox.PlayOneShot("ui/UIEnter"); });
+            musicSource.Play();
+        }
+
+        private void Update()
+        {
+            if (songPos >= musicSource.clip.length)
+            {
+                time = 0;
+                targetBopBeat = 0;
+            }
+            double absTime = Time.realtimeSinceStartup;
+            double dt = absTime - lastAbsTime;
+            lastAbsTime = absTime;
+
+            time += dt;
+
+            songPos = time;
+
+            songPosBeat = SecsToBeats(songPos);
+
+            if (songPosBeat >= targetBopBeat)
+            {
+                logoAnim.Play("LogoBop", 0, 0);
+                targetBopBeat += 1;
+            }
+        }
+
+        public double SecsToBeats(double s)
+        {
+            return s / 60f * bpm;
         }
     }
 }
