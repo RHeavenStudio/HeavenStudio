5ca54ba7b7decb3f88c7f8c12abc6c0f5ebaafd9
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-05-18 23:41:34 +0200

    Added sfx to title screen

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using HeavenStudio.Util;
 
 namespace HeavenStudio
 {
     public class TitleManager : MonoBehaviour
     {
-        [SerializeField] Button createButton;
+        [SerializeField] private Button createButton;
+
+        [SerializeField] private float Bpm = 114f;
         private void Start()
         {
-            createButton.onClick.AddListener(delegate { GlobalGameManager.LoadScene("Editor"); });
+            createButton.onClick.AddListener(delegate { GlobalGameManager.LoadScene("Editor"); Jukebox.PlayOneShot("ui/UIEnter"); });
         }
     }
 }
