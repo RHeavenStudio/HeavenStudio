3a8769c2a411d4834b5bfcd0369d6a304cb6a7b2
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-06-13 23:29:30 +0200

    Synchrettes now spawn in and stuff

---
+++ Assets/Scripts/Games/Splashdown/NtrSynchrette.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HeavenStudio.Util;
+using System;
+
+namespace HeavenStudio.Games.Scripts_Splashdown
+{
+    public class NtrSynchrette : MonoBehaviour
+    {
+
+    }
+}
+
--- Assets/Scripts/Games/Splashdown/Splashdown.cs
+++ Assets/Scripts/Games/Splashdown/Splashdown.cs
@@ -21,13 +21,45 @@ namespace HeavenStudio.Games.Loaders
 
 namespace HeavenStudio.Games
 {
+    using Scripts_Splashdown;
     public class Splashdown : Minigame
     {
         public static Splashdown instance;
+        [Header("References")]
+        [SerializeField] private Transform synchretteHolder;
+        [SerializeField] private NtrSynchrette synchrettePrefab;
+        [Header("Properties")]
+        [SerializeField] private float synchretteDistance;
+
+        private List<NtrSynchrette> currentSynchrettes = new List<NtrSynchrette>();
+        private NtrSynchrette player;
 
         private void Awake()
         {
             instance = this;
+            SpawnSynchrettes(3);
+        }
+
+        public void SpawnSynchrettes(int amount)
+        {
+            if (currentSynchrettes.Count > 0)
+            {
+                foreach (var synchrette in currentSynchrettes)
+                {
+                    Destroy(synchrette.gameObject);
+                }
+                currentSynchrettes.Clear();
+            }
+            if (player != null) Destroy(player.gameObject);
+            float startPos = -Mathf.Floor(amount / 2) * synchretteDistance;
+
+            for (int i = 0; i < amount; i++)
+            {
+                NtrSynchrette spawnedSynchrette = Instantiate(synchrettePrefab, synchretteHolder);
+                spawnedSynchrette.transform.localPosition = new Vector3(startPos + (synchretteDistance * i), 0, 0);
+                if (i < amount - 1) currentSynchrettes.Add(spawnedSynchrette);
+                else player = spawnedSynchrette;
+            }
         }
     }
 }
