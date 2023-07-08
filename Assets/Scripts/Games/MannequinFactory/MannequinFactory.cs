c96646745c4e157125d1de7b6d66ce4e5367c308
AstrlJelly <bdlawson115@gmail.com>
2023-07-07 23:43:25 -0400

    everything
    yeah. everything..?
    just needs an icon

---
+++ Assets/Scripts/Games/MannequinFactory/MannequinFactory.cs
@@ -0,0 +1,140 @@
+using System.Collections.Generic;
+using HeavenStudio.Util;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+namespace HeavenStudio.Games.Loaders
+{
+    using static Minigames;
+    public static class AgbMannequinFactoryLoader
+    {
+        public static Minigame AddGame(EventCaller eventCaller) {
+            return new Minigame("mannequinFactory", "Mannequin Factory", "554899", false, false, new List<GameAction>()
+            {
+                new GameAction("headOut", "Send Head Out")
+                {
+                    function = delegate {
+                        var e = eventCaller.currentEntity;
+                        MannequinFactory.instance.HeadOut(e.beat, e["type"]);
+                    }, 
+                    defaultLength = 7,
+                    parameters = new List<Param>()
+                    {
+                        new Param("type", MannequinFactory.HeadOutTypes.NoClap, "Cue Type", "Should the mannequin go out wrong or not?"),
+                    }
+                },
+                new GameAction("changeText", "Change Text")
+                {
+                    function = delegate { MannequinFactory.instance.ChangeSignText(eventCaller.currentEntity["text"]); }, 
+                    defaultLength = 0.5f,
+                    parameters = new List<Param>()
+                    {
+                        new Param("text", "Mannequin Factory", "Text", "The text to be displayed on the sign"),
+                    }
+                },
+                new GameAction("bgColor", "Change Background Color")
+                {
+                    function = delegate {
+                        var e = eventCaller.currentEntity;
+                        MannequinFactory.instance.BackgroundColor(e["start"], e["end"], e.length, e["instant"]);
+                    },
+                    parameters = new List<Param>()
+                    {
+                        new Param("start", new Color(0.97f, 0.94f, 0.51f, 1f), "Start Color", "The color to start fading from."),
+                        new Param("end",   new Color(0.97f, 0.94f, 0.51f, 1f), "End Color", "The color to end the fade."),
+                        new Param("instant", false, "Instant", "If checked, the background color will instantly change to the start color.")
+                    },
+                    resizable = true
+                },
+            }/*,
+            new List<string>() {"agb", "normal"},
+            "agbmannequin", "en",
+            new List<string>() {}*/
+            );
+        }
+    }
+}
+
+namespace HeavenStudio.Games
+{
+    using Scripts_MannequinFactory;
+    public class MannequinFactory : Minigame
+    {
+        [Header("Animators")]
+        public Animator HandAnim;
+        public Animator StampAnim;
+        
+        [Header("References")]
+        [SerializeField] SpriteRenderer bg;
+        [SerializeField] TMP_Text SignText;
+        public GameObject MannequinHeadObject;
+        Tween bgColorTween;
+
+        public enum HeadOutTypes
+        {
+            Random,
+            NoClap,
+            Clap,
+        }
+
+        public static MannequinFactory instance;
+        
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void Update()
+        {
+            if (PlayerInput.Pressed() && !IsExpectingInputNow(InputType.STANDARD_DOWN))
+            {
+                StampAnim.DoScaledAnimationAsync("StampEmpty");
+            }
+
+            if (PlayerInput.Pressed(true) && PlayerInput.GetSpecificDirection(3) && !IsExpectingInputNow(InputType.DIRECTION_LEFT_DOWN)) 
+            {
+                HandAnim.DoScaledAnimationAsync("Slap");
+            }
+        }
+
+        public void HeadOut(double beat, int cueType)
+        {
+            if (cueType == 0) cueType = UnityEngine.Random.Range(1, 3);
+            var sfx = new List<MultiSound.Sound>() {
+                new MultiSound.Sound("mannequinFactory/drum", beat      ),
+                new MultiSound.Sound("mannequinFactory/drum", beat + 0.5),
+                new MultiSound.Sound("mannequinFactory/drum", beat + 1.5),
+                new MultiSound.Sound("mannequinFactory/drum", beat + 2  ),
+            };
+            if (cueType == 1) {
+                for (int i = 0; i < 7; i++) {
+                    sfx.Add(new MultiSound.Sound($"mannequinFactory/drumroll{i + 1}", beat + 3 + (i * 0.1667)));
+                }
+            } else {
+                sfx.AddRange(new MultiSound.Sound[] {
+                    new MultiSound.Sound("mannequinFactory/drum", beat + 0.75),
+                    new MultiSound.Sound("mannequinFactory/drum", beat + 1   ),
+                });
+            }
+            MultiSound.Play(sfx.ToArray());
+
+            MannequinHead head = Instantiate(MannequinHeadObject, gameObject.transform).GetComponent<MannequinHead>();
+            head.startBeat = beat;
+            head.withClap = (cueType == 2);
+        }
+
+        public void ChangeSignText(string text)
+        {
+            SignText.text = text;
+        }
+
+        public void BackgroundColor(Color start, Color end, float beats, bool instant)
+        {
+            if (bgColorTween != null) bgColorTween.Kill(true);
+
+            bg.color = instant ? end : start;
+            if (!instant) bgColorTween = bg.DOColor(end, Conductor.instance.secPerBeat * beats);
+        }
+    }
+}
---
+++ Assets/Scripts/Games/MannequinFactory/MannequinHead.cs
@@ -0,0 +1,146 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+using HeavenStudio.Util;
+
+namespace HeavenStudio.Games.Scripts_MannequinFactory
+{
+    public class MannequinHead : MonoBehaviour
+    {
+        public double startBeat;
+        public bool withClap;
+        public bool clapped;
+        
+        [Header("Animators")]
+        [SerializeField] SpriteRenderer headSr;
+        [SerializeField] Sprite[] heads;
+        [SerializeField] SpriteRenderer eyesSr;
+        [SerializeField] Sprite[] eyes;
+        [SerializeField] Animator headAnim;
+        Animator stampAnim;
+        Animator handAnim;
+
+        int turnStatus;
+
+        private MannequinFactory game;
+        
+        private void Awake()
+        {
+            game = MannequinFactory.instance;
+            
+            stampAnim = game.StampAnim;
+            handAnim = game.HandAnim;
+        }
+
+        private void Start() 
+        {
+            turnStatus = withClap ? 0 : 1;
+            clapped = !withClap;
+            headSr.sprite = heads[turnStatus];
+
+            BeatAction.New(gameObject, new List<BeatAction.Action> {
+                new BeatAction.Action(startBeat + 1, delegate { headAnim.DoScaledAnimationAsync("Move1", 0.3f); }),
+                new BeatAction.Action(startBeat + 3, delegate { headAnim.DoScaledAnimationAsync("Move2", 0.3f); }),
+                new BeatAction.Action(startBeat + 4, delegate {
+                    if (turnStatus == 1) game.ScheduleInput(startBeat, 5, InputType.STANDARD_DOWN, StampJust, StampMiss, Nothing);
+                    else game.ScheduleUserInput(startBeat, 5, InputType.STANDARD_DOWN, StampUnJust, StampMiss, Nothing);
+                }),
+            });
+            if (withClap) SoundByte.PlayOneShotGame("mannequinFactory/whoosh", beat: startBeat + 3);
+            SoundByte.PlayOneShotGame("mannequinFactory/whoosh", beat: startBeat + 5);
+
+            if (withClap) game.ScheduleInput(startBeat, 3, InputType.DIRECTION_LEFT_DOWN, ClapJust, ClapMiss, Nothing);
+            else      game.ScheduleUserInput(startBeat, 3, InputType.DIRECTION_LEFT_DOWN, ClapUnJust, ClapMiss, Nothing);
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        void ClapJust(PlayerActionEvent caller, float state)
+        {
+            ClapHit(state);
+            headSr.sprite = heads[turnStatus];
+        }
+
+        void ClapUnJust(PlayerActionEvent caller, float state)
+        { 
+            eyesSr.transform.localScale = new Vector2(-1, 1);
+            headSr.transform.localScale = new Vector2(-1, 1);
+            headSr.sprite = heads[0];
+            game.ScoreMiss();
+            ClapHit(state);
+        }
+
+        void ClapHit(float state)
+        {
+            turnStatus++;
+            SoundByte.PlayOneShotGame("mannequinFactory/slap");
+            handAnim.DoScaledAnimationAsync("Slap");
+            headAnim.Play("Slapped", 0, 0);
+            clapped = true;
+        }
+
+        void ClapMiss(PlayerActionEvent caller)
+        {
+            //headAnim.DoScaledAnimationAsync("Move2", 0.3f);
+        }
+
+        void StampHit(float state)
+        {
+            if (state >= 1f || state <= -1f) SoundByte.PlayOneShot("nearMiss");
+            headAnim.DoScaledAnimationAsync("Stamp", 0.3f);
+            stampAnim.DoScaledAnimationAsync("StampJust", 0.3f);
+            SoundByte.PlayOneShotGame("mannequinFactory/eyes");
+            eyesSr.gameObject.SetActive(true);
+        }
+
+        void StampJust(PlayerActionEvent caller, float state)
+        {
+            StampHit(state);
+
+            MultiSound.Play(new MultiSound.Sound[] {
+                new MultiSound.Sound("mannequinFactory/claw1", startBeat + 6),
+                new MultiSound.Sound("mannequinFactory/claw2", startBeat + 6.5),
+            });
+            BeatAction.New(gameObject, new List<BeatAction.Action> {
+                new BeatAction.Action(startBeat + 5.75, delegate { headAnim.DoScaledAnimationAsync("Grabbed1", 0.3f); }),
+                new BeatAction.Action(startBeat + 6   , delegate { headAnim.DoScaledAnimationAsync("Grabbed2", 0.3f); }),
+            });
+        }
+
+        void StampUnJust(PlayerActionEvent caller, float state)
+        {
+            StampHit(state);
+            eyesSr.sprite = eyes[1];
+
+            headAnim.DoScaledAnimationAsync("Stamp", 0.3f);
+            BeatAction.New(gameObject, new List<BeatAction.Action> {
+                new BeatAction.Action(startBeat + 6, delegate {
+                    SoundByte.PlayOneShotGame("mannequinFactory/miss");
+                    headAnim.DoScaledAnimationAsync("StampMiss", 0.3f);
+                }),
+            });
+        }
+
+        void StampMiss(PlayerActionEvent caller)
+        {
+            headAnim.DoScaledAnimationAsync("Move3", 0.3f);
+            BeatAction.New(gameObject, new List<BeatAction.Action> {
+                new BeatAction.Action(startBeat + 6.5, delegate {
+                    SoundByte.PlayOneShotGame("mannequinFactory/miss");
+                    headAnim.DoScaledAnimationAsync("Miss", 0.3f);
+                }),
+            });
+        }
+
+        void Nothing(PlayerActionEvent caller) { }
+
+        // animation event
+        public void DestroySelf()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
