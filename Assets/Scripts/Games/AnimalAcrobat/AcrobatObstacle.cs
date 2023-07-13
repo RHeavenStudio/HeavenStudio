34f6be44d69604a166d5da75587a5db8ed28d219
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2023-07-13 23:08:31 +0200

    animals spawning logic

---
+++ Assets/Scripts/Games/AnimalAcrobat/AcrobatObstacle.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HeavenStudio.Util;
+
+namespace HeavenStudio.Games.Scripts_AnimalAcrobat
+{
+    public class AcrobatObstacle : MonoBehaviour
+    {
+        private enum ObstacleType
+        {
+            Elephant,
+            Giraffe,
+            Monkeys,
+            Monkey
+        }
+        [Header("Properties")]
+        [SerializeField] private float holdLength = 2f;
+        [SerializeField] private float fullRotateAngle = 120f;
+        [SerializeField] private Animator anim;
+        [SerializeField] private ObstacleType type;
+        [SerializeField] private Transform rotatePivot;
+        private double startBeat = double.MinValue;
+        private float halfAngle;
+        private EasingFunction.Function func;
+
+        private void Awake()
+        {
+            halfAngle = fullRotateAngle / 2;
+            func = EasingFunction.GetEasingFunction(EasingFunction.Ease.EaseInOutQuad);
+        }
+
+        public void Init(double beat, double gameSwitchBeat)
+        {
+            startBeat = beat;
+            MultiSound.Play(new MultiSound.Sound[]
+            {
+                new MultiSound.Sound("animalAcrobat/eek", startBeat + holdLength - 2),
+                new MultiSound.Sound("animalAcrobat/eek", startBeat + holdLength - 1),
+            });
+            if (type is ObstacleType.Elephant or ObstacleType.Giraffe)
+            {
+                bool isElephant = type is ObstacleType.Elephant;
+                BeatAction.New(gameObject, new List<BeatAction.Action>()
+                {
+                    new BeatAction.Action(startBeat + holdLength - 2, delegate
+                    {
+                        anim.DoScaledAnimationAsync(isElephant ? "ElephantEar" : "GiraffeEar", 0.5f);
+                    }),
+                    new BeatAction.Action(startBeat + holdLength - 1, delegate
+                    {
+                        anim.DoScaledAnimationAsync(isElephant ? "ElephantEar" : "GiraffeEar", 0.5f);
+                    }),
+                });
+            }
+            Update();
+        }
+
+        private void Update()
+        {
+            var cond = Conductor.instance;
+            if (startBeat != double.MinValue)
+            {
+                SwingUpdate(cond);
+            }
+        }
+
+        private void SwingUpdate(Conductor cond)
+        {
+            float normalizedSwingBeat = cond.GetPositionFromBeat(startBeat, holdLength);
+            float negativeOffset = (normalizedSwingBeat < 0) ? -1 : 0;
+            if ((Mathf.Floor(normalizedSwingBeat) + negativeOffset) % 2 == 0)
+            {
+                rotatePivot.localEulerAngles = new Vector3(0, 0, func(-halfAngle, halfAngle, Mathf.Abs(normalizedSwingBeat % 1)));
+            }
+            else
+            {
+                rotatePivot.localEulerAngles = new Vector3(0, 0, func(halfAngle, -halfAngle, Mathf.Abs(normalizedSwingBeat % 1)));
+            }
+        }
+    }
+}
+
+
--- Assets/Scripts/Games/AnimalAcrobat/AnimalAcrobat.cs
+++ Assets/Scripts/Games/AnimalAcrobat/AnimalAcrobat.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using HeavenStudio.Util;
+using HeavenStudio.Games.Scripts_AnimalAcrobat;
+using Jukebox;
 
 namespace HeavenStudio.Games.Loaders
 {
@@ -12,7 +14,26 @@ namespace HeavenStudio.Games.Loaders
         {
             return new Minigame("animalAcrobat", "Animal Acrobat", "FFFFFF", false, false, new List<GameAction>()
             {
-
+                new GameAction("elephant", "Elephant")
+                {
+                    defaultLength = 4
+                },
+                new GameAction("giraffe", "Giraffe")
+                {
+                    defaultLength = 8
+                },
+                new GameAction("monkeys", "Monkeys")
+                {
+                    defaultLength = 8
+                },
+                new GameAction("monkeyLong", "Monkey Line Standalone")
+                {
+                    defaultLength = 5
+                },
+                new GameAction("monkeyShort", "Monkey Standalone")
+                {
+                    defaultLength = 3
+                }
             });
         }
     }
@@ -22,11 +43,100 @@ namespace HeavenStudio.Games
 {
     public class AnimalAcrobat : Minigame
     {
+        [Header("Elephant")]
+        [SerializeField] private AcrobatObstacle elephantRef;
+        [SerializeField] private float elephantDistance;
+        [SerializeField] private float elephantStart = 0.004f;
+        [Header("Giraffe")]
+        [SerializeField] private AcrobatObstacle giraffeRef;
+        [SerializeField] private float giraffeDistance;
+        [SerializeField] private float giraffeStart;
+        [Header("Monkeys")]
+        [SerializeField] private AcrobatObstacle monkeysRef;
+        [SerializeField] private float monkeysDistance;
+        [SerializeField] private float monkeysStart;
+        [Header("One Monkey")]
+        [SerializeField] private AcrobatObstacle oneMonkeyRef;
+        [SerializeField] private float oneMonkeyDistance;
+        [SerializeField] private float oneMonkeyStart;
+
         public static AnimalAcrobat instance;
 
         private void Awake()
         {
             instance = this;
+        }
+
+        public override void OnPlay(double beat)
+        {
+            SpawnAnimals(beat);
+        }
+
+        public override void OnGameSwitch(double beat)
+        {
+            SpawnAnimals(beat);
+        }
+
+        private void SpawnAnimals(double gameSwitchBeat)
+        {
+            var animalEvents = EventCaller.GetAllInGameManagerList("animalAcrobat", new string[] { "elephant", "giraffe", "monkeys", "monkeyLong", "monkeyShort"});
+            if (animalEvents.Count == 0) return;
+            animalEvents.Sort((x, y) => x.beat.CompareTo(y.beat));
+            double nextBeat = animalEvents[0].beat;
+            float startPos = animalEvents[0].datamodel switch
+            {
+                "animalAcrobat/elephant" => elephantStart - elephantDistance,
+                "animalAcrobat/giraffe" => giraffeStart - giraffeDistance,
+                "animalAcrobat/monkeys" => monkeysStart - monkeysDistance,
+                "animalAcrobat/monkeyLong" => monkeysStart - monkeysDistance,
+                "animalAcrobat/monkeyShort" => oneMonkeyStart - oneMonkeyDistance,
+                _ => throw new System.NotImplementedException()
+            }; 
+            foreach (var animal in animalEvents)
+            {
+                if (animal.beat == nextBeat)
+                {
+                    nextBeat += animal.length;
+                    switch (animal.datamodel)
+                    {
+                        case "animalAcrobat/elephant":
+                            startPos += elephantDistance;
+                            AcrobatObstacle spawnedElephant = Instantiate(elephantRef, transform);
+                            spawnedElephant.transform.position = new Vector3(startPos, 0, 0);
+                            spawnedElephant.Init(animal.beat, gameSwitchBeat);
+                            break;
+                        case "animalAcrobat/giraffe":
+                            startPos += giraffeDistance;
+                            AcrobatObstacle spawnedGiraffe = Instantiate(giraffeRef, transform);
+                            spawnedGiraffe.transform.position = new Vector3(startPos, 0, 0);
+                            spawnedGiraffe.Init(animal.beat, gameSwitchBeat);
+                            break;
+                        case "animalAcrobat/monkeys":
+                            startPos += monkeysDistance;
+                            AcrobatObstacle spawnedMonkeyLong = Instantiate(monkeysRef, transform);
+                            spawnedMonkeyLong.transform.position = new Vector3(startPos, 0, 0);
+                            spawnedMonkeyLong.Init(animal.beat, gameSwitchBeat);
+                            startPos += oneMonkeyDistance;
+                            AcrobatObstacle spawnedOneMonkeyS = Instantiate(oneMonkeyRef, transform);
+                            spawnedOneMonkeyS.transform.position = new Vector3(startPos, 0, 0);
+                            spawnedOneMonkeyS.Init(animal.beat, gameSwitchBeat);
+                            break;
+                        case "animalAcrobat/monkeyLong":
+                            startPos += monkeysDistance;
+                            AcrobatObstacle spawnedMonkeys = Instantiate(monkeysRef, transform);
+                            spawnedMonkeys.transform.position = new Vector3(startPos, 0, 0);
+                            spawnedMonkeys.Init(animal.beat, gameSwitchBeat);
+                            break;
+                        case "animalAcrobat/monkeyShort":
+                            startPos += oneMonkeyDistance;
+                            AcrobatObstacle spawnedOneMonkey = Instantiate(oneMonkeyRef, transform);
+                            spawnedOneMonkey.transform.position = new Vector3(startPos, 0, 0);
+                            spawnedOneMonkey.Init(animal.beat, gameSwitchBeat);
+                            break;
+                        default: break;
+                    }
+                }
+            }
         }
     }
 }
