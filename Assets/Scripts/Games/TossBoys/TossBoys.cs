e975b3a20c95e5c6f60967de50587c95aea295e9
ThatZeoMan <67521686+ThatZeoMan@users.noreply.github.com>
2024-01-14 10:47:01 -0600

    Tram & Pauline Text Update
    also added translation for Fireworks

--- Assets/Scripts/Games/TossBoys/TossBoys.cs
+++ Assets/Scripts/Games/TossBoys/TossBoys.cs
@@ -12,7 +12,7 @@ namespace HeavenStudio.Games.Loaders
     {
         public static Minigame AddGame(EventCaller eventCaller)
         {
-            return new Minigame("tossBoys", "Toss Boys", "9cfff7", false, false, new List<GameAction>()
+            return new Minigame("tossBoys", "Toss Boys \n<color=#adadad>(Tosu Bōizu)</color>", "9cfff7", false, false, new List<GameAction>()
             {
                 new GameAction("bop", "Bop")
                 {
