1a94838cd9725bcfafecf83a28e998574037556e
blank3times <105398129+blank3times@users.noreply.github.com>
2024-01-22 22:57:52 -0800

    Update HoleInOne.cs

--- Assets/Scripts/Games/HoleInOne/HoleInOne.cs
+++ Assets/Scripts/Games/HoleInOne/HoleInOne.cs
@@ -18,7 +18,14 @@ namespace HeavenStudio.Games.Loaders
         {
             return new Minigame("holeInOne", "Hole in One", "6ab99e", false, false, new List<GameAction>()
             {
-            }
+                // new GameAction("testanims", "Test Animation")
+                // {
+                //     function = delegate { HoleInOne.instance.DoTestAnim(eventCaller.currentEntity.beat); },
+                // }
+            },
+            new List<string>() { "rvl", "normal" },
+            "rvlgolf", "en",
+            new List<string>() { }
             );
         }
     }
@@ -30,5 +37,20 @@ namespace HeavenStudio.Games
     /// Minigame inherits directly from MonoBehaviour, and adds Heaven Studio specific methods to override.
     public class HoleInOne : Minigame
     {
+        // public Animator Monkey;
+
+        // public static HoleInOne instance;
+
+        // public void DoTestAnim(double beat)
+        // {
+        //     //Bell Sound lol
+        //     SoundByte.PlayOneShotGame("rhythmSomen/somen_bell");
+
+        //     BeatAction.New(this, new List<BeatAction.Action>()
+        //     {
+        //     new BeatAction.Action(beat,     delegate { Monkey.DoScaledAnimationAsync("MonkeySpin", 0.5f);}),
+        //     });
+
+        // }
     }
 }
