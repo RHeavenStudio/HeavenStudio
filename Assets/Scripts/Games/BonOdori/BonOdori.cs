b29fb5363d0f99c1ca6123fdeb4f4b68152da725
streitixy <danilomeiraivo@gmail.com>
2024-01-25 16:56:05 -0300

    bon odori is now playable, just no animations and sounds

--- Assets/Scripts/Games/BonOdori/BonOdori.cs
+++ Assets/Scripts/Games/BonOdori/BonOdori.cs
@@ -12,24 +12,7 @@ using Jukebox;
 namespace HeavenStudio.Games.Loaders
 {
     using static Minigames;
-    /// Minigame loaders handle the setup of your minigame.
-    /// Here, you designate the game prefab, define entities, and mark what AssetBundle to load
 
-    /// Names of minigame loaders follow a specific naming convention of `PlatformcodeNameLoader`, where:
-    /// `Platformcode` is a three-leter platform code with the minigame's origin
-    /// `Name` is a short internal name
-    /// `Loader` is the string "Loader"
-
-    /// Platform codes are as follows:
-    /// Agb: Gameboy Advance    ("Advance Gameboy")
-    /// Ntr: Nintendo DS        ("Nitro")
-    /// Rvl: Nintendo Wii       ("Revolution")
-    /// Ctr: Nintendo 3DS       ("Centrair")
-    /// Mob: Mobile
-    /// Pco: PC / Other
-
-    /// Fill in the loader class label, "*prefab name*", and "*Display Name*" with the relevant information
-    /// For help, feel free to reach out to us on our discord, in the #development channel.
     public static class AgbBonOdoriLoader
     {
         public static Minigame AddGame(EventCaller eventCaller)
@@ -38,9 +21,21 @@ namespace HeavenStudio.Games.Loaders
             {
                 new GameAction("pan", "Pan")
                 {
-                    function = delegate { BonOdori.instance.Clap(eventCaller.currentEntity.beat); },
+                    function = delegate { BonOdori.instance.Clap(eventCaller.currentEntity.beat, 1); },
                     defaultLength = 2f,
+                },
+                new GameAction("pa-n", "Pa-n")
+                {
+                    function = delegate { BonOdori.instance.Clap(eventCaller.currentEntity.beat, 1.5); },
+                    defaultLength = 2.5f,
+                },
+                    new GameAction("pa", "Pa")
+                {
+                    function = delegate { BonOdori.instance.Clap(eventCaller.currentEntity.beat, 0.5 ); },
+                    defaultLength = 1.5f,
                 }
+
+
 
             
             
@@ -58,43 +53,54 @@ namespace HeavenStudio.Games
 
 
 
-    /// This class handles the minigame logic.
-    /// Minigame inherits directly from MonoBehaviour, and adds Heaven Studio specific methods to override.
+
     public class BonOdori : Minigame
     {
         
-        [Header("Animators")]
-        public Animator AnimatorGeneral;
+        [Header("Animations")]
+        public Animator AnimatorPlayer;
+        public Animator AnimatorDonpan1;
+        public Animator AnimatorDonpan2;
+        public Animator AnimatorDonpan3;
+        
+        
         public static BonOdori instance { get; set; }
+        
+        
         public void Awake()
+        
         {
-
+            instance = this;
+            Debug.Log("AWAKEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
         }
-        public void Clap(double beat)
+        public void Clap(double beat, double type)
         {   
-            ScheduleInput(beat, 1f, InputAction_BasicPress, CatchSuccess, CatchMiss, CatchEmpty);
-            AnimatorGeneral.Play("ClapGeneral", 0, 0);
-            new MultiSound.Sound("coinToss/cowbell2", 1f, offset: 0.01f);
+            ScheduleInput(beat, beat + 1f * type, InputAction_BasicPress, Success, Miss, Empty);
+            Debug.Log("TESTE");
+
 
         }
 
-        public void CatchSuccess(PlayerActionEvent caller, float state)
+        public void Success(PlayerActionEvent caller, float state)
         {
-        AnimatorGeneral.Play("ClapGeneral", 0, 0);
-        new MultiSound.Sound("coinToss/cowbell2", 1f, offset: 0.01f);
+            AnimatorPlayer.Play("ClapAll", 0, 0);
+            AnimatorDonpan1.Play("ClapAll", 0, 0);
+            AnimatorDonpan2.Play("ClapAll", 0, 0);
+            AnimatorDonpan3.Play("ClapAll", 0, 0);
+            Debug.Log("SUCCESS");
         }
         
-        public void CatchMiss(PlayerActionEvent caller)
+        public void Miss(PlayerActionEvent caller)
         {
-        AnimatorGeneral.Play("ClapGeneral", 0, 0);
-        new MultiSound.Sound("coinToss/cowbell2", 1f, offset: 0.01f);
+            Debug.Log("MISS");
+
         }
         
         
-        public void CatchEmpty(PlayerActionEvent caller)
+        public void Empty(PlayerActionEvent caller)
         {
-        AnimatorGeneral.Play("ClapGeneral", 0, 0);
-        new MultiSound.Sound("coinToss/cowbell2", 1f, offset: 0.01f);
+            Debug.Log("Empty");
+
         }
         
 
