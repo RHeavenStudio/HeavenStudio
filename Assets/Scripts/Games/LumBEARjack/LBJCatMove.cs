1cb2d5d74cc41c41bc43d455ea1a0ae42cfdca18
Rapandrasmus <78219215+Rapandrasmus@users.noreply.github.com>
2024-02-13 22:45:07 +0100

    slide offset

--- Assets/Scripts/Games/LumBEARjack/LBJCatMove.cs
+++ Assets/Scripts/Games/LumBEARjack/LBJCatMove.cs
@@ -12,15 +12,26 @@ namespace HeavenStudio.Games.Scripts_LumBEARjack
 
         [Header("Properties")]
         [SerializeField] private bool _startAtOther;
+        [SerializeField] private bool _usePoint = true;
+        [SerializeField] private float _slideOffset = 0f;
 
         private Vector3 _thisPosition;
+        private Vector3 _otherPosition;
 
         private Coroutine _currentMove;
 
         private void Awake()
         {
             _thisPosition = transform.position;
-            if (_startAtOther) transform.position = _otherPoint.position;
+            if (_usePoint)
+            {
+                _otherPosition = _otherPoint.position;
+            }
+            else
+            {
+                _otherPosition = _thisPosition += new Vector3(_slideOffset, 0);
+            }
+            if (_startAtOther) transform.position = _otherPosition;
         }
 
         public void Move(double beat, double length, bool inToScene)
@@ -33,14 +44,14 @@ namespace HeavenStudio.Games.Scripts_LumBEARjack
         {
             if (length <= 0)
             {
-                transform.position = inToScene ? _thisPosition : _otherPoint.position;
+                transform.position = inToScene ? _thisPosition : _otherPosition;
                 yield break;
             }
             float normalized = Conductor.instance.GetPositionFromBeat(beat, length, false);
             while (normalized <= 1f)
             {
                 normalized = Conductor.instance.GetPositionFromBeat(beat, length);
-                Vector3 newPos = Vector3.Lerp(inToScene ? _otherPoint.position : _thisPosition, inToScene ? _thisPosition : _otherPoint.position, normalized);
+                Vector3 newPos = Vector3.Lerp(inToScene ? _otherPosition : _thisPosition, inToScene ? _thisPosition : _otherPosition, normalized);
                 transform.position = newPos;
                 yield return null;
             }
